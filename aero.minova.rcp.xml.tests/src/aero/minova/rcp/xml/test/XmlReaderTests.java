c551a1ad187250bad90d6fdfe4f5c83a5657b147
Lars Vogel <Lars.Vogel@vogella.com>
2020-06-26 14:45:29 +0200

    Adding a small unit test

---
+++ aero.minova.rcp.xml.tests/src/aero/minova/rcp/xml/test/XmlReaderTests.java
@@ -0,0 +1,12 @@
+package aero.minova.rcp.xml.test;
+
+import static org.junit.Assert.*;
+
+
+public class XmlReaderTests {
+	@org.junit.Test
+	public void testName() {
+		assertTrue(false);
+	}
+	
+}
