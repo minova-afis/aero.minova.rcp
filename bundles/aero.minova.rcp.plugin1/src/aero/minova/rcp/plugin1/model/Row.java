696118fcff9c1b2827c6a75c7b6bc7c6bf49fc32
Christopher Erlanger <erlanger@minova.com>
2020-07-28 14:52:45 +0200

    Erweiterungen

--- bundles/aero.minova.rcp.plugin1/src/aero/minova/rcp/plugin1/model/Row.java
+++ bundles/aero.minova.rcp.plugin1/src/aero/minova/rcp/plugin1/model/Row.java
@@ -1,12 +1,20 @@
 package aero.minova.rcp.plugin1.model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 public class Row {
-	List<Value> values = new Vector<>();
+	List<Value> values = new ArrayList<>();
 
 	public void addValue(Value v) {
 		values.add(v);
+	}
+
+	public Value getValue(int index) {
+		return values.get(index);
+	}
+
+	public void setValue(Value value, int index) {
+		values.set(index, value);
 	}
 }
