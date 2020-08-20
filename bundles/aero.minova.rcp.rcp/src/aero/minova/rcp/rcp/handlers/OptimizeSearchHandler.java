4b04d4b35a8b69d4e11192d425d113b1b6bd3dce
BADMinova <61147549+BADMinova@users.noreply.github.com>
2020-08-20 10:31:08 +0200

    organisiert Imports

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeSearchHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeSearchHandler.java
@@ -1,10 +1,7 @@
 package aero.minova.rcp.rcp.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
-
-import aero.minova.rcp.dialogs.PasswordDialog;
 
 public class OptimizeSearchHandler {
 	
