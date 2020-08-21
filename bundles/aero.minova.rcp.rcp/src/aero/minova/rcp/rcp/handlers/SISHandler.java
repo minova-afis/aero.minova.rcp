0dff2c29aed6583cfa4f13a7a72218f90b8cb29b
BADMinova <61147549+BADMinova@users.noreply.github.com>
2020-08-21 11:34:32 +0200

    fügt Handler für SIS Perspektive hinzu

---
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/SISHandler.java
@@ -0,0 +1,26 @@
+
+package aero.minova.rcp.rcp.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+import aero.minova.rcp.perspectiveswitcher.handler.SwitchPerspectiveHandler;
+
+public class SISHandler extends SwitchPerspectiveHandler {
+
+	@Inject
+	EModelService modelService;
+
+	@Execute
+	public void execute(MWindow window, MApplication app) throws InvocationTargetException, InterruptedException {
+
+		execute(window.getContext(), "aero.minova.rcp.rcp.perspective.sis", "org.eclipse.e4.ui.perspectives.parameters.newWindow", window);
+	}
+
+}
