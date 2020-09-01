a23fe3df50c94a5601ce652513c8e2dbd5cd82de
kuemmert <kuemmert@localhost>
2020-09-01 08:40:18 +0200

    Added an event for the indextable, if a row is selected a request will be send to the CAS, requesting the data that fits the key-long. This data will be visible in the detailview, this works due to databinding.

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/parts/XMLIndexPart.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/parts/XMLIndexPart.java
@@ -1,5 +1,9 @@
 package aero.minova.rcp.rcp.parts;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -8,15 +12,24 @@ import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.layer.ILayerListener;
+import org.eclipse.nebula.widgets.nattable.layer.event.ILayerEvent;
+import org.eclipse.nebula.widgets.nattable.selection.event.CellSelectionEvent;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-
 import aero.minova.rcp.dataservice.IDataFormService;
+import aero.minova.rcp.dataservice.IDataService;
 import aero.minova.rcp.dataservice.IMinovaJsonService;
+import aero.minova.rcp.plugin1.model.Column;
+import aero.minova.rcp.plugin1.model.DataType;
 import aero.minova.rcp.form.model.xsd.Form;
 import aero.minova.rcp.plugin1.model.Row;
 import aero.minova.rcp.plugin1.model.Table;
+import aero.minova.rcp.plugin1.model.Value;
 import aero.minova.rcp.rcp.util.NatTableUtil;
 import aero.minova.rcp.rcp.util.PersistTableSelection;
 
@@ -32,10 +45,22 @@ public class XMLIndexPart {
 	@Inject
 	private IDataFormService dataFormService;
 
+	@Inject
+	private EModelService model;
+
 	private Table data;
 
 	@Inject
 	IEventBroker broker;
+
+	@Inject
+	private IDataService dataService;
+
+	@Inject
+	private MPart mPart;
+
+	@Inject
+	private MPerspective mPerspective;
 
 	private NatTable natTable;
 
@@ -54,6 +79,15 @@ public class XMLIndexPart {
 
 		parent.setLayout(new GridLayout());
 		natTable = NatTableUtil.createNatTable(parent, form, data, true);
+		natTable.addLayerListener(new ILayerListener() {
+			@Override
+			public void handleLayerEvent(ILayerEvent arg0) {
+				if (arg0 instanceof CellSelectionEvent) {
+					changeSelectedEntry(
+							natTable.getLayer().getRowIndexByPosition(((CellSelectionEvent) arg0).getRowPosition()));
+				}
+			}
+		});
 	}
 
 	@PersistTableSelection
@@ -74,5 +108,43 @@ public class XMLIndexPart {
 		}
 		natTable.refresh(false);
 		natTable.requestLayout();
+	}
+
+	public void changeSelectedEntry(int selectedRowIndex) {
+		int keylong = (int) natTable.getLayer().getDataValueByPosition(1, selectedRowIndex + 1);
+		System.out.println(keylong);
+		//
+		Table rowIndexTable = new Table();
+		rowIndexTable.setName("spReadWorkingTime");
+		rowIndexTable.addColumn(new Column("KeyLong", DataType.INTEGER));
+		rowIndexTable.addColumn(new Column("EmployeeKey", DataType.STRING));
+		rowIndexTable.addColumn(new Column("OrderReceiverKey", DataType.STRING));
+		rowIndexTable.addColumn(new Column("ServiceContractKey", DataType.STRING));
+		rowIndexTable.addColumn(new Column("ServiceObjectKey", DataType.STRING));
+		rowIndexTable.addColumn(new Column("ServiceKey", DataType.STRING));
+		rowIndexTable.addColumn(new Column("BookingDate", DataType.ZONED));
+		rowIndexTable.addColumn(new Column("StartDate", DataType.ZONED));
+		rowIndexTable.addColumn(new Column("EndDate", DataType.ZONED));
+		rowIndexTable.addColumn(new Column("RenderedQuantity", DataType.DOUBLE));
+		rowIndexTable.addColumn(new Column("ChargedQuantity", DataType.DOUBLE));
+		rowIndexTable.addColumn(new Column("Description", DataType.STRING));
+		rowIndexTable.addColumn(new Column("Spelling", DataType.STRING));
+		Row r = new Row();
+		r.addValue(new Value(keylong));
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		r.addValue(null);
+		rowIndexTable.addRow(r);
+		CompletableFuture<Table> tableFuture = dataService.getDataAsync(rowIndexTable.getName(), rowIndexTable);
+		tableFuture.thenAccept(t -> broker.post("CAS_request_selected_entry", t));
 	}
 }
