553a3007c00d436e389078402ba072be2063cabd
David Bauer <bauer@minova.com>
2020-09-02 09:54:18 +0200

    fügt den KeepIt und Close Handler Parameter hinzu

--- bundles/aero.minova.rcp.perspectiveswitcher/src/aero/minova/rcp/perspectiveswitcher/handler/KeepPerspectiveHandler.java
+++ bundles/aero.minova.rcp.perspectiveswitcher/src/aero/minova/rcp/perspectiveswitcher/handler/KeepPerspectiveHandler.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+import aero.minova.rcp.perspectiveswitcher.commands.E4WorkbenchParameterConstants;
 
 /**
  * Dieser Handler wird im PopupMenu aufgerufen. Toolitems, die mit diesem
@@ -35,23 +38,21 @@ public class KeepPerspectiveHandler {
 
 	@SuppressWarnings("unchecked")
 	@Execute
-	public void execute(MWindow window) {
+	public void execute(MWindow window, @Optional @Named(E4WorkbenchParameterConstants.COMMAND_PERSPECTIVE_ID) String perspectiveId) {
 
 		List<String> keepItToolitems;
 
-		MPerspective currentPerspective = modelService.getActivePerspective(window);
-
 		keepItToolitems = (List<String>) application.getContext().get("perspectivetoolbar");
-
+		
 		if (keepItToolitems == null) {
 			keepItToolitems = new ArrayList<String>();
 			application.getContext().set("perspectivetoolbar", keepItToolitems);
 		}
 
-		if (keepItToolitems.contains(currentPerspective.getElementId())) {
-			keepItToolitems.remove(currentPerspective.getElementId());
+		if (keepItToolitems.contains(perspectiveId)) {
+			keepItToolitems.remove(perspectiveId);
 		} else {
-			keepItToolitems.add(currentPerspective.getElementId());
+			keepItToolitems.add(perspectiveId);
 		}
 
 	}
--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClosePerspectiveHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClosePerspectiveHandler.java
@@ -3,8 +3,10 @@ package aero.minova.rcp.rcp.handlers;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspectiveStack;
@@ -12,9 +14,13 @@ import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
+import aero.minova.rcp.perspectiveswitcher.commands.E4WorkbenchParameterConstants;
 import aero.minova.rcp.perspectiveswitcher.handler.SwitchPerspectiveHandler;
 
-public class ClosePerspectiveHandler extends SwitchPerspectiveHandler  {
+/**
+ * 
+ */
+public class ClosePerspectiveHandler extends SwitchPerspectiveHandler {
 
 	@Inject
 	MApplication application;
@@ -26,20 +32,18 @@ public class ClosePerspectiveHandler extends SwitchPerspectiveHandler  {
 	EPartService partService;
 
 	@Execute
-	public void execute(MWindow window) {
+	public void execute(MWindow window,
+			@Optional @Named(E4WorkbenchParameterConstants.COMMAND_PERSPECTIVE_ID) String perspectiveId) {
 
-		/*
-		 * Holt die in dem Moment aktive Perspektive.
-		 */
-		MPerspective currentPerspective = modelService.getActivePerspective(window);
+		List<MPerspective> perspective = modelService.findElements(application, perspectiveId, MPerspective.class);
 
 		/*
 		 * Entfernt die aktuelle Perspektive.
 		 */
 
-		MPerspectiveStack perspectiveStack = (MPerspectiveStack) modelService.find("aero.minova.rcp.rcp.perspectivestack", application);
-		perspectiveStack.getChildren().remove(currentPerspective);
-
+		MPerspectiveStack perspectiveStack = (MPerspectiveStack) modelService
+				.find("aero.minova.rcp.rcp.perspectivestack", application);
+		perspectiveStack.getChildren().remove(perspective.get(0));
 
 		/*
 		 * Wechselt zur Perspektive, die in der PerspektiveList den Index 0 hat.
