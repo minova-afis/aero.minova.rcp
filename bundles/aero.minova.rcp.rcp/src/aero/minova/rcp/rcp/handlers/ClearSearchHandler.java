c52205d5e26aec2dfde572c3e7047f2821bd99ea
kuemmert <kuemmert@localhost>
2020-09-10 09:08:13 +0200

    Implementation of the ClearSearchHandler

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClearSearchHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClearSearchHandler.java
@@ -1,14 +1,28 @@
 package aero.minova.rcp.rcp.handlers;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+import aero.minova.rcp.core.ui.PartsID;
+import aero.minova.rcp.plugin1.model.Table;
 
 public class ClearSearchHandler {
-	
+
+	@Inject
+	EModelService model;
+
 	@Execute
-	public void execute(Shell shell) {
-		System.out.println("Clear NatTable Search Part");
-		
+	public void execute(MPerspective mPerspective) {
+		List<MPart> findElements = model.findElements(mPerspective, PartsID.SEARCH_PART, MPart.class);
+		Table table = (Table) findElements.get(0).getContext().get("NatTableDataSearchArea");
+		table.getRows().clear();
+		table.addRow();
 	}
 
 }
