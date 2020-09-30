b5c06a1a6bb47e20ea4035b2d5dddaf3e91e97b5
Christopher Erlanger <erlanger@minova.com>
2020-09-30 16:32:03 +0200

    weitere Tests für TextfieldVerifier

---
+++ tests/aero.minova.rcp.xml.tests/src/aero/minova/rcp/xml/tests/TextfieldVerifierTest.java
@@ -0,0 +1,58 @@
+package aero.minova.rcp.xml.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+import aero.minova.rcp.rcp.util.TextfieldVerifier;
+
+public class TextfieldVerifierTest {
+
+	String heute;
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+	@Test
+	public void test0() {
+		String verifyTime = TextfieldVerifier.verifyDate("0");
+		assertEquals(LocalDate.now().format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testh() {
+		String verifyTime = TextfieldVerifier.verifyDate("h");
+		assertEquals(LocalDate.now().format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testH() {
+		String verifyTime = TextfieldVerifier.verifyDate("H");
+		assertEquals(LocalDate.now().format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testm() {
+		String verifyTime = TextfieldVerifier.verifyDate("m");
+		assertEquals(LocalDate.now().plusDays(1).format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testPlusPlusPlus() {
+		String verifyTime = TextfieldVerifier.verifyDate("+++");
+		assertEquals(LocalDate.now().plusDays(3).format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testMinus() {
+		String verifyTime = TextfieldVerifier.verifyDate("-");
+		assertEquals(LocalDate.now().minusDays(1).format(formatter), verifyTime);
+	}
+
+	@Test
+	public void testMinusMinusMinusMinus() {
+		String verifyTime = TextfieldVerifier.verifyDate("----");
+		assertEquals(LocalDate.now().minusDays(4).format(formatter), verifyTime);
+	}
+}
