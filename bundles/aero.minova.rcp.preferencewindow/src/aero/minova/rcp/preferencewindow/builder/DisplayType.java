b729e92221838c6a13519a1943730c525c5c1392
David Bauer <bauer@minova.com>
2020-10-06 16:23:36 +0200

    ergänzt DisplayType für TimeZone Widget

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/builder/DisplayType.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/builder/DisplayType.java
@@ -1,5 +1,5 @@
 package aero.minova.rcp.preferencewindow.builder;
 
 public enum DisplayType {
-	FILE, DIRECTORY, INTEGER, FLOAT, STRING, COMBO, CHECK, URL, PASSWORD, TEXT, FONT,
+	FILE, DIRECTORY, INTEGER, FLOAT, STRING, COMBO, CHECK, URL, PASSWORD, TEXT, FONT, ZONEID
 }
