a74fdd95e23c6da7dc8fc95843a52a080f7d828c
David Bauer <bauer@minova.com>
2020-10-07 11:06:36 +0200

    holt alle möglichen Locales

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/CustomLocale.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/CustomLocale.java
@@ -1,7 +1,14 @@
 package aero.minova.rcp.preferencewindow.control;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class CustomLocale {
 
-
+	public static Locale[] getLocales() {
+		return SimpleDateFormat.getAvailableLocales();
+	}
 
 }
