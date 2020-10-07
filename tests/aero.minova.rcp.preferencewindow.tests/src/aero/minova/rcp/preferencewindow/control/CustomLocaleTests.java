76bc80ac77b1e97f40d58c996499e654c6292650
David Bauer <bauer@minova.com>
2020-10-07 11:09:46 +0200

    testet getLocale auf null und Länge

--- tests/aero.minova.rcp.preferencewindow.tests/src/aero/minova/rcp/preferencewindow/control/CustomLocaleTests.java
+++ tests/aero.minova.rcp.preferencewindow.tests/src/aero/minova/rcp/preferencewindow/control/CustomLocaleTests.java
@@ -1,5 +1,18 @@
 package aero.minova.rcp.preferencewindow.control;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
 public class CustomLocaleTests {
-	
+
+	@Test
+	public void testGetLocales() {
+		Locale list[] = CustomLocale.getLocales();
+		assertNotNull(list);
+		assertEquals(748, list.length);
+	}
 }
