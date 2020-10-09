d82bd616a1f47440c55a7476b68a0c5ccf071fb3
David Bauer <bauer@minova.com>
2020-10-09 10:25:43 +0200

    LOCALE DisplayType hinzugefügt

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/builder/DisplayType.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/builder/DisplayType.java
@@ -1,5 +1,5 @@
 package aero.minova.rcp.preferencewindow.builder;
 
 public enum DisplayType {
-	FILE, DIRECTORY, INTEGER, FLOAT, STRING, COMBO, CHECK, URL, PASSWORD, TEXT, FONT, ZONEID
+	FILE, DIRECTORY, INTEGER, FLOAT, STRING, COMBO, CHECK, URL, PASSWORD, TEXT, FONT, ZONEID, LOCALE
 }
