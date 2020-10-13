7b36c28d22cff1c72b14410c7e25697505615aa4
David Bauer <bauer@minova.com>
2020-10-13 15:40:11 +0200

    Liste mit Ländern wird beim Wechseln der Sprache neu gebaut

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/PWLocale.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/PWLocale.java
@@ -1,6 +1,11 @@
 package aero.minova.rcp.preferencewindow.control;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.nebula.widgets.opal.preferencewindow.PreferenceWindow;
@@ -9,7 +14,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.osgi.service.prefs.BackingStoreException;
+import org.eclipse.swt.widgets.Label;
 import org.osgi.service.prefs.Preferences;
 
 import aero.minova.rcp.preferencewindow.builder.DisplayType;
@@ -18,18 +23,21 @@ import aero.minova.rcp.preferencewindow.builder.InstancePreferenceAccessor;
 public class PWLocale extends CustomPWWidget {
 	Preferences preferences = InstanceScope.INSTANCE.getNode("aero.minova.rcp.preferencewindow");
 
-	private List<String> dataC = CustomLocale.getCountries("language");
+	private Map<String, Object> data;
 	private final List<String> dataL = CustomLocale.getLanguages();
 	private final boolean editable;
+
+	private Combo comboCountries;
 
 	/**
 	 * Constructor
 	 *
-	 * @param label       associated label
-	 * @param propertyKey associated key
+	 * @param languageLabel associated label
+	 * @param propertyKey   associated key
 	 */
-	public PWLocale(final String label, final String propertyKey, final Object... values) {
-		this(label, propertyKey, false, values);
+	public PWLocale(final String languageLabel, String countryLabel, final String propertyKey, Map<String, Object> data,
+			final Object... values) {
+		this(languageLabel, propertyKey, false, data, values);
 	}
 
 	/**
@@ -38,9 +46,23 @@ public class PWLocale extends CustomPWWidget {
 	 * @param label       associated label
 	 * @param propertyKey associated key
 	 */
-	public PWLocale(final String label, final String propertyKey, final boolean editable, final Object... values) {
+	public PWLocale(final String label, final String propertyKey, final boolean editable, Map<String, Object> data,
+			final Object... values) {
 		super(label, propertyKey, label == null ? 1 : 2, false);
 		this.editable = editable;
+	}
+
+	public List<String> getCountriesByData() {
+		Locale locales[] = CustomLocale.getLocales();
+		data = PreferenceWindow.getInstance().getValues();
+		List<String> countries = new ArrayList<>();
+		for (Locale locale : locales) {
+			if (!locale.getDisplayCountry().equals("") && !countries.contains(locale.getDisplayCountry())
+					&& data.get(getCustomPropertyKey()).toString().equals(locale.getDisplayLanguage()))
+				countries.add(locale.getDisplayCountry());
+		}
+		Collections.sort(countries);
+		return countries;
 	}
 
 	/**
@@ -48,49 +70,65 @@ public class PWLocale extends CustomPWWidget {
 	 */
 	@Override
 	public Control build(final Composite parent) {
-		buildLabel(parent, GridData.CENTER);
+		final Label languageLabel = new Label(parent, SWT.NONE);
+		languageLabel.setText("Sprache");
+		final GridData labelLGridData = new GridData(GridData.END, getAlignment(), false, false);
+		labelLGridData.horizontalIndent = getIndent();
+		languageLabel.setLayoutData(labelLGridData);
+		addControl(languageLabel);
 
-		final Combo combo = new Combo(parent, SWT.BORDER | (editable ? SWT.NONE : SWT.READ_ONLY));
-		addControl(combo);
+		final Combo comboLanguage = new Combo(parent, SWT.BORDER | (editable ? SWT.NONE : SWT.READ_ONLY));
+		addControl(comboLanguage);
 
 		for (int i = 0; i < dataL.size(); i++) {
 			final Object datum = dataL.get(i);
-			combo.add(datum.toString());
+			comboLanguage.add(datum.toString());
 			if (datum.equals(PreferenceWindow.getInstance().getValueFor("language"))) {
-				combo.select(i);
+				comboLanguage.select(i);
 			}
 		}
 
-		combo.addListener(SWT.Modify, event -> {
+		comboLanguage.addListener(SWT.Modify, event -> {
 			PreferenceWindow.getInstance().setValue(getCustomPropertyKey(),
-					PWLocale.this.dataL.get(combo.getSelectionIndex()));
-			InstancePreferenceAccessor.putValue(preferences, getCustomPropertyKey(), DisplayType.COMBO,
-					PWLocale.this.dataL.get(combo.getSelectionIndex()));
-			try {
-				preferences.flush();
-			} catch (BackingStoreException e) {
-				e.printStackTrace();
+					PWLocale.this.dataL.get(comboLanguage.getSelectionIndex()));
+			comboCountries.removeAll();
+			for (String string : getCountriesByData()) {
+				comboCountries.add(string);
+				if (string.equals(PreferenceWindow.getInstance().getValueFor("land"))) {
+					comboCountries.select(0);
+				}
 			}
+			InstancePreferenceAccessor.putValue(preferences, "language", DisplayType.LOCALE,
+					PWLocale.this.getCountriesByData().get(comboLanguage.getSelectionIndex()));
+
 		});
 
-		buildLabel(parent, GridData.CENTER);
+		final Label countryLabel = new Label(parent, SWT.NONE);
+		countryLabel.setText("Land");
+		final GridData labelCGridData = new GridData(GridData.END, getAlignment(), false, false);
+		labelCGridData.horizontalIndent = getIndent();
+		countryLabel.setLayoutData(labelCGridData);
+		addControl(countryLabel);
 
-		final Combo comboC = new Combo(parent, SWT.BORDER | (editable ? SWT.NONE : SWT.READ_ONLY));
-		addControl(comboC);
+		comboCountries = new Combo(parent, SWT.BORDER | (editable ? SWT.NONE : SWT.READ_ONLY));
+		addControl(comboCountries);
 
-		for (int i = 0; i < dataC.size(); i++) {
-			final Object datum = dataC.get(i);
-			comboC.add(datum.toString());
+		for (int i = 0; i < getCountriesByData().size(); i++) {
+			final Object datum = getCountriesByData().get(i);
+			comboCountries.add(datum.toString());
 			if (datum.equals(PreferenceWindow.getInstance().getValueFor("land"))) {
-				comboC.select(i);
+				comboCountries.select(i);
 			}
 		}
 
-		comboC.addListener(SWT.Modify, event -> {
-			PreferenceWindow.getInstance().setValue("land", PWLocale.this.dataL.get(combo.getSelectionIndex()));
+		comboCountries.addListener(SWT.Modify, event -> {
+			PreferenceWindow.getInstance().setValue("land",
+					PWLocale.this.getCountriesByData().get(comboCountries.getSelectionIndex()));
+			InstancePreferenceAccessor.putValue(preferences, "land", DisplayType.LOCALE,
+					PWLocale.this.getCountriesByData().get(comboCountries.getSelectionIndex()));
 		});
 
-		return combo;
+		return comboLanguage;
 	}
 
 	/**
@@ -107,10 +145,10 @@ public class PWLocale extends CustomPWWidget {
 						+ "' has to be a String because it is associated to an editable combo");
 			}
 
-			if (!dataC.isEmpty()) {
-				if (!value.getClass().equals(dataC.get(0).getClass())) {
+			if (!getCountriesByData().isEmpty()) {
+				if (!value.getClass().equals(getCountriesByData().get(0).getClass())) {
 					throw new UnsupportedOperationException("The property '" + getCustomPropertyKey() + "' has to be a "
-							+ dataC.get(0).getClass() + " because it is associated to a combo");
+							+ getCountriesByData().get(0).getClass() + " because it is associated to a combo");
 				}
 			}
 
