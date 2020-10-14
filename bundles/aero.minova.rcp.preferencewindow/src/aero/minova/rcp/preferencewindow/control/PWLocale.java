70a30985a5720ff180ada3260e766074a1521315
David Bauer <bauer@minova.com>
2020-10-14 08:18:39 +0200

    RF: Imports organisiert

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/PWLocale.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/control/PWLocale.java
@@ -2,7 +2,6 @@ package aero.minova.rcp.preferencewindow.control;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
