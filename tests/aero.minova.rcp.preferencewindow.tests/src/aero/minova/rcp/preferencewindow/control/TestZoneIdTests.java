a9ae731b065fee95df2c717e3002a1acae9677a9
David Bauer <bauer@minova.com>
2020-10-16 11:14:59 +0200

    Tests an alle Änderungen von Methoden anpassen

--- tests/aero.minova.rcp.preferencewindow.tests/src/aero/minova/rcp/preferencewindow/control/TestZoneIdTests.java
+++ tests/aero.minova.rcp.preferencewindow.tests/src/aero/minova/rcp/preferencewindow/control/TestZoneIdTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.ZoneId;
-import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
 import java.util.Locale.Category;
@@ -21,55 +20,40 @@ public class TestZoneIdTests {
 
 	@Test
 	public void testNotNull() {
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(Locale.GERMAN);
+		Map<String, ZoneId> zones = CustomTimeZone.getZones();
 		assertNotNull(zones);
 	}
 
 	@Test
 	public void testGermanZones() {
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(Locale.GERMAN);
-		assertEquals(198, zones.size());
+		Map<String, ZoneId> zones = CustomTimeZone.getZones();
+		assertEquals(196, zones.size());
 	}
 
 	@Test
 	public void testGermanyZones() {
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(Locale.GERMANY);
-		assertEquals(198, zones.size());
-	}
-
-	@Test
-	public void testGetId() {
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(Locale.GERMANY);
-		assertNotNull(CustomTimeZone.getId(zones, "Mitteleuropäische Zeit", Locale.GERMAN));
-		assertEquals("Europe/Monaco", CustomTimeZone.getId(zones, "Mitteleuropäische Zeit", Locale.GERMAN).toString());
+		Map<String, ZoneId> zones = CustomTimeZone.getZones();
+		assertEquals(196, zones.size());
 	}
 
 	@Test
 	public void testGetIdENGLISH() {
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(Locale.US);
+		Map<String, ZoneId> zones = CustomTimeZone.getZones();
 		assertNotNull(CustomTimeZone.getId(zones, "Central European Time", Locale.US));
 		assertEquals("Europe/Monaco", CustomTimeZone.getId(zones, "Central European Time", Locale.US).toString());
 	}
 
 	@Test
-	public void testGetTimeZone() {
-		String id = "Europe/Monaco";
-		String tz = ZoneId.of(id).getDisplayName(TextStyle.FULL, Locale.GERMAN);
-		String result = CustomTimeZone.displayTimeZone(Locale.GERMAN, tz);
-		assertEquals("(GMT+1:00) Mitteleuropäische Zeit", result);
-	}
-
-	@Test
 	public void testListSize() {
 		List<String> zones = CustomTimeZone.getTimeZones();
-		assertEquals(198, zones.size());
+		assertEquals(196, zones.size());
 	}
 
 	@Test
 	public void testGetTimeZoneId() {
 		Object value = "Ulyanovsk Time";
 		Locale l = Locale.getDefault(Category.DISPLAY);
-		Map<String, ZoneId> zones = CustomTimeZone.getZones(l);
+		Map<String, ZoneId> zones = CustomTimeZone.getZones();
 		String id = value.toString().substring(value.toString().lastIndexOf(")") + 1);
 		String zoneId = CustomTimeZone.getId(zones, id, l).toString();
 		assertEquals("Europe/Ulyanovsk", zoneId);
