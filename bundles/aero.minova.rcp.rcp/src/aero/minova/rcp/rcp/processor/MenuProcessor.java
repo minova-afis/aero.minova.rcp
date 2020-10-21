466d76f3d581d934693ffcb0634108e0e55d5054
Christopher Erlanger <erlanger@minova.com>
2020-10-21 15:22:16 +0200

    plugin für das einlesen der MDI Dateien

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/processor/MenuProcessor.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/processor/MenuProcessor.java
@@ -1,8 +1,15 @@
 package aero.minova.rcp.rcp.processor;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
@@ -17,6 +24,30 @@ public class MenuProcessor {
 		System.out.println("Starting to process the model " + menu);
 		// TODO Download and parse file
 		// dataService.getFile(null);
+		// dataService.getFile("mdi.xml");
+
+		String basePath = null;
+		try {
+			basePath = Platform.getInstanceLocation().getURL().toURI().toString();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		basePath = basePath + "mdi.xml";
+
+		URI uri = null;
+		try {
+			uri = new URI(basePath);
+		} catch (URISyntaxException e1) {
+		}
+
+		try {
+			String content = new String(Files.readAllBytes(Path.of(uri)));
+			System.out.println(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// TODO
+		// Parsen der MDI Datei um die Menus zu generieren
 
 		MMenu menuGen = modelService.createModelElement(MMenu.class);
 		menuGen.setLabel("Generate1");
