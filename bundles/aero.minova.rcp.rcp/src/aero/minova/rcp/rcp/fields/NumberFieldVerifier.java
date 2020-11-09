ee277b146ca564ae4492c377d8af08d5a24fe3c3
David Bauer <bauer@minova.com>
2020-11-09 10:41:04 +0100

    NumberFieldVerifier Klasse geeignet für Test umbauen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/fields/NumberFieldVerifier.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/fields/NumberFieldVerifier.java
@@ -10,56 +10,80 @@ import org.eclipse.swt.widgets.Text;
 
 public class NumberFieldVerifier implements VerifyListener {
 
-	private Text field;
-	private int decimals;
 	private boolean verificationActive = false;
 
-	public NumberFieldVerifier(Text text) {
-		field = text;
-		decimals = (int) field.getData(FieldUtil.FIELD_DECIMALS);
+	public NumberFieldVerifier() {
 	}
 
 	@Override
 	public void verifyText(VerifyEvent e) {
-		if (verificationActive) return; // Wir setzen gerade den Wert
-		if (e.widget != field) return; // ist nicht unser Feld
-
+		Text field = (Text) e.getSource();
+		int decimals = (int) field.getData(FieldUtil.FIELD_DECIMALS);
 		Locale locale = (Locale) field.getData(FieldUtil.TRANSLATE_LOCALE);
-		DecimalFormatSymbols dfs = new DecimalFormatSymbols(locale);
-		NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
-		numberFormat.setMaximumFractionDigits(decimals);
-		numberFormat.setMinimumFractionDigits(decimals);
-		numberFormat.setGroupingUsed(true);
-		String insertion = e.text;
-		String text = field.getText();
-		if (e.text.equals("" + dfs.getDecimalSeparator()) && text.indexOf(dfs.getDecimalSeparator()) >= 0) {
-			field.setSelection(text.indexOf(dfs.getDecimalSeparator()) + 1);
-			e.doit = false;
-			return;
-		}
-
 		int caretPosition = field.getCaretPosition();
-		String newText;
-		if ("".equals(e.text)) {
-			newText = text.substring(0, e.start) + text.substring(e.end);
-		} else {
-			newText = text.substring(0, caretPosition) + insertion + text.substring(caretPosition);
-		}
-		newText = newText.replaceAll("[" + dfs.getGroupingSeparator() + "]", "");
-		newText = newText.replaceAll("[" + dfs.getDecimalSeparator() + "]", ".");
-		Double newValue;
-		if (newText.isEmpty()) {
-			newValue = null;
-		} else {
-			newValue = Double.parseDouble(newText);
-			newText = numberFormat.format(newValue);
-			verificationActive = true;
-			field.setText(newText);
-			field.setSelection(caretPosition + insertion.length());
-			verificationActive = false;
-		}
-		field.setData(FieldUtil.FIELD_VALUE, newValue);
-		e.doit = false;
+		int start = e.start;
+		int end = e.end;
+		String textBefore = field.getText();
+		String insertion = e.text;
+		
+		String newText = getNewText(decimals, locale, textBefore, caretPosition, start, end, insertion);
+		int newCaretPosition = getNewCaretPosition(decimals, locale, textBefore, caretPosition, start, end, insertion, newText);
+		
+			}
+
+	
+//	{
+//		if (verificationActive) return; // Wir setzen gerade den Wert
+//		if (e.widget != field) return; // ist nicht unser Feld
+//
+//		DecimalFormatSymbols dfs = new DecimalFormatSymbols(locale);
+//		NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+//		numberFormat.setMaximumFractionDigits(decimals);
+//		numberFormat.setMinimumFractionDigits(decimals);
+//		numberFormat.setGroupingUsed(true);
+//		String insertion = e.text;
+//		String text = field.getText();
+//		if (e.text.equals("" + dfs.getDecimalSeparator()) && text.indexOf(dfs.getDecimalSeparator()) >= 0) {
+//			field.setSelection(text.indexOf(dfs.getDecimalSeparator()) + 1);
+//			e.doit = false;
+//			return;
+//		}
+//
+//		int caretPosition = field.getCaretPosition();
+//		String newText;
+//		if ("".equals(e.text)) {
+//			newText = text.substring(0, e.start) + text.substring(e.end);
+//		} else {
+//			newText = text.substring(0, caretPosition) + insertion + text.substring(caretPosition);
+//		}
+//		newText = newText.replaceAll("[" + dfs.getGroupingSeparator() + "]", "");
+//		newText = newText.replaceAll("[" + dfs.getDecimalSeparator() + "]", ".");
+//		Double newValue;
+//		if (newText.isEmpty()) {
+//			newValue = null;
+//		} else {
+//			newValue = Double.parseDouble(newText);
+//			newText = numberFormat.format(newValue);
+//			verificationActive = true;
+//			field.setText(newText);
+//			field.setSelection(caretPosition + insertion.length());
+//			verificationActive = false;
+//		}
+//		field.setData(FieldUtil.FIELD_VALUE, newValue);
+//		e.doit = false;
+//
+//	}
+	
+	protected int getNewCaretPosition(int decimals, Locale locale, String textBefore, int caretPosition, int start,
+			int end, String insertion, String newText) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	protected String getNewText(int decimals, Locale locale, String textBefore, int caretPosition, int start, int end,
+			String insertion) {
+		// TODO Auto-generated method stub
+		return "9,00";
 	}
 
 }
