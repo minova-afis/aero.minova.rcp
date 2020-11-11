945801f1688ef5ecf84aecfd13402618f101e2a0
kuemmert <kuemmert@localhost>
2020-11-11 09:13:47 +0100

    Anpassen der sql-Statements zum erstellen der Tabelle, dem einfügen und dem löschen von Datensätzen

--- bundles/aero.minova.rcp.dataservice/src/aero/minova/rcp/dataservice/internal/LocalDatabaseService.java
+++ bundles/aero.minova.rcp.dataservice/src/aero/minova/rcp/dataservice/internal/LocalDatabaseService.java
@@ -3,7 +3,9 @@ package aero.minova.rcp.dataservice.internal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.Platform;
@@ -11,8 +13,11 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
 import aero.minova.rcp.dataservice.ILocalDatabaseService;
+import aero.minova.rcp.model.Column;
+import aero.minova.rcp.model.DataType;
 import aero.minova.rcp.model.Row;
 import aero.minova.rcp.model.Table;
+import aero.minova.rcp.model.Value;
 
 @Component
 public class LocalDatabaseService implements ILocalDatabaseService {
@@ -29,9 +34,9 @@ public class LocalDatabaseService implements ILocalDatabaseService {
 
 	private Properties properties = new Properties();
 
-//	private Statement s;
-//
-//	private ResultSet rs = null;
+	private Statement s;
+
+	private ResultSet rs = null;
 
 	private Connection conn;
 
@@ -46,6 +51,11 @@ public class LocalDatabaseService implements ILocalDatabaseService {
 			Class.forName("org.h2.Driver");
 			String dbUrl = protocol + Platform.getInstanceLocation().getURL().getPath().toString() + "/cache/lookup";
 			conn = DriverManager.getConnection(dbUrl);
+
+			conn.setAutoCommit(false);
+			createTableIfNotExisting = conn.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS Lookupvalues ( Lookupvalue_id IDENTITY NOT NULL PRIMARY KEY, Lookup VARCHAR NOT NULL, KeyLong int NOT NULL, KeyText VARCHAR NOT NULL, DescriptionText VARCHAR)");
+			createTableIfNotExisting.execute();
 		} catch (SQLException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -56,32 +66,43 @@ public class LocalDatabaseService implements ILocalDatabaseService {
 
 	@Deactivate
 	protected void deactivateComponent() {
-//		entityManager.close();
-//		entityManagerFactory.close();
-//		entityManager = null;
-//		entityManagerFactory = null;
 		disconnect();
 	}
 
 	@Override
 	public Table getResultsForLookupField(String name) {
-		/*
-		 * Connection conn = connect(); if (conn != null) { Table t = new Table();
-		 * Column collumn = new Column("KeyLong", DataType.INTEGER);
-		 * t.addColumn(collumn); collumn = new Column("KeyText", DataType.STRING);
-		 * t.addColumn(collumn); collumn = new Column("Description", DataType.STRING);
-		 * t.addColumn(collumn); try { rs =
-		 * s.executeQuery("SELECT * FROM Lookupvalues WHERE Lookup =" + name); if
-		 * (!rs.next()) { return null; } else { while (rs.next()) { Row r = new Row();
-		 * r.addValue(new Value(rs.getInt(3), DataType.INTEGER)); r.addValue(new
-		 * Value(rs.getInt(4), DataType.STRING)); r.addValue(new Value(rs.getInt(5),
-		 * DataType.STRING)); t.addRow(r); } } } catch (SQLException e) {
-		 * System.out.println("Auslesen der Datenbank fehlgeschlagen.");
-		 * e.printStackTrace(); return null; } disconnect(); return t; } else { return
-		 * null; }
-		 */
 
-		return null;
+		activateComponent();
+		if (conn != null) {
+			Table t = new Table();
+			Column collumn = new Column("KeyLong", DataType.INTEGER);
+			t.addColumn(collumn);
+			collumn = new Column("KeyText", DataType.STRING);
+			t.addColumn(collumn);
+			collumn = new Column("Description", DataType.STRING);
+			t.addColumn(collumn);
+			try {
+				rs = s.executeQuery("SELECT * FROM Lookupvalues WHERE Lookup ='" + name + "'");
+				if (!rs.next()) {
+					return null;
+				} else {
+					while (rs.next()) {
+						Row r = new Row();
+						r.addValue(new Value(rs.getInt(3), DataType.INTEGER));
+						r.addValue(new Value(rs.getInt(4), DataType.STRING));
+						r.addValue(new Value(rs.getInt(5), DataType.STRING));
+						t.addRow(r);
+					}
+				}
+			} catch (SQLException e) {
+				System.out.println("Auslesen der Datenbank fehlgeschlagen.");
+				e.printStackTrace();
+				return null;
+			}
+			return t;
+		} else {
+			return null;
+		}
 	}
 
 	@Override
@@ -91,16 +112,17 @@ public class LocalDatabaseService implements ILocalDatabaseService {
 		if (conn != null) {
 			try {
 				insertEntryOfLookup = conn.prepareStatement(
-						"INSERT INTO Lookupvalues(Lookup varchar(40), KeyLong int, KeyText varchar(40), Description varchar(40))");
-				deleteAllEntriesOfLookup = conn.prepareStatement("DELETE FROM Lookupvalues WHERE Lookup=" + name);
+						"INSERT INTO Lookupvalues(Lookup, KeyLong, KeyText, DescriptionText) VALUES (?, ?, ?, ?)");
+				deleteAllEntriesOfLookup = conn
+						.prepareStatement("DELETE FROM Lookupvalues WHERE Lookup='" + name + "'");
 				int i = 0;
 
 				while (i < table.getRows().size()) {
 					Row r = table.getRows().get(i);
-					insertEntryOfLookup.setString(2, name);
-					insertEntryOfLookup.setInt(3, r.getValue(table.getColumnIndex("KeyLong")).getIntegerValue());
-					insertEntryOfLookup.setString(4, r.getValue(table.getColumnIndex("KeyText")).getStringValue());
-					insertEntryOfLookup.setString(5, r.getValue(table.getColumnIndex("Description")).getStringValue());
+					insertEntryOfLookup.setString(1, name);
+					insertEntryOfLookup.setInt(2, r.getValue(table.getColumnIndex("KeyLong")).getIntegerValue());
+					insertEntryOfLookup.setString(3, r.getValue(table.getColumnIndex("KeyText")).getStringValue());
+					insertEntryOfLookup.setString(4, r.getValue(table.getColumnIndex("Description")).getStringValue());
 
 					i++;
 				}
@@ -110,32 +132,9 @@ public class LocalDatabaseService implements ILocalDatabaseService {
 				System.out.println("Austausch der Werte der Datenbank fehlgeschlagen.");
 				e.printStackTrace();
 			}
-			disconnect();
 		}
 
 	}
-
-//	private Connection connect() {
-//		Connection conn = null;
-//		try {
-//			properties.put("user", "user1");
-//			properties.put("password", "password");
-//			conn = DriverManager.getConnection(protocol + database + ";create=true", properties);
-//			conn.setAutoCommit(false);
-//			createTableIfNotExisting = conn.prepareStatement("CREATE TABLE IF NOT EXISTS Lookupvalues (" + //
-//					"	Lookupvalue_id int NOT NULL AUTO_INCREMENT," + //
-//					"	Lookup varchar(40)," + //
-//					"	KeyLong int," + //
-//					"	KeyText varchar(40)" + //
-//					"	Description varchar(40)," + //
-//					"	PRIMARY KEY (Lookupvalues_id))");
-//			createTableIfNotExisting.execute();
-//		} catch (SQLException e) {
-//			System.out.println("Verbindung zur Datenbank fehlgeschlagen.");
-//			e.printStackTrace();
-//		}
-//		return conn;
-//	}
 
 	private void disconnect() {
 		try {
