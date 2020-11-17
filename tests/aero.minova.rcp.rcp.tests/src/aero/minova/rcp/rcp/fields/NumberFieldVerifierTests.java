892908b6df6814e735ef45882f01793913d346b4
David Bauer <bauer@minova.com>
2020-11-17 09:18:56 +0100

    Tests ergänzen

--- tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/fields/NumberFieldVerifierTests.java
+++ tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/fields/NumberFieldVerifierTests.java
@@ -2,6 +2,7 @@ package aero.minova.rcp.rcp.fields;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 import org.junit.Test;
@@ -9,11 +10,28 @@ import org.junit.Test;
 public class NumberFieldVerifierTests {
 	
 	@Test
-	public void testDecimalSeparator() {
+	public void testDecimalSeparatorGerman() {
+		NumberFieldVerifier nfv = new NumberFieldVerifier();
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
+		
+		assertEquals("9.000,00", nfv.getNewText(2, Locale.GERMANY, "", 0, 0, 0, "9000", dfs));
+		assertEquals(0, nfv.getNewCaretPosition(2, Locale.GERMANY, "", 0, 0, 0, "9", "9,00"));
+	}
+	
+	@Test
+	public void testDecimalSeparatorEnglish() {
+		NumberFieldVerifier nfv = new NumberFieldVerifier();
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
+		
+		assertEquals("9,000.00", nfv.getNewText(2, Locale.US, "", 0, 0, 0, "9000", dfs));
+		assertEquals(0, nfv.getNewCaretPosition(2, Locale.US, "", 0, 0, 0, "9", "9,00"));
+	}
+	
+	@Test
+	public void testGetNewValue() {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		
-		assertEquals("9,00", nfv.getNewText(2, Locale.GERMANY, "", 0, 0, 0, "9"));
-		assertEquals(1, nfv.getNewCaretPosition(2, Locale.GERMANY, "", 0, 0, 0, "9", "9,00"));
+		assertEquals(Double.valueOf(9000.0), nfv.getNewValue("9000.0"));
 	}
 
 }
