4743300e28839e058c84026c32afe8c73d604df6
David Bauer <bauer@minova.com>
2020-11-20 16:02:55 +0100

    Test für keyCode 8 und 127 hinzufügen

--- tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/fields/NumberFieldVerifierTests.java
+++ tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/fields/NumberFieldVerifierTests.java
@@ -39,7 +39,7 @@ public class NumberFieldVerifierTests {
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMAN);
 
 		assertEquals("get text", "1,00", nfv.getNewText(2, Locale.GERMANY, "0,00", 1, 1, 1, "1", dfs));
-		assertEquals("get caret", 1, nfv.getNewCaretPosition("0,00", "1", dfs, 1));
+		assertEquals("get caret", 1, nfv.getNewCaretPosition("0,00", "1", "1,00", dfs, 1, 1));
 	}
 
 	@Test
@@ -48,7 +48,7 @@ public class NumberFieldVerifierTests {
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMAN);
 
 		assertEquals("get text", "12,00", nfv.getNewText(2, Locale.GERMANY, "1,00", 1, 1, 1, "2", dfs));
-		assertEquals("get caret", 2, nfv.getNewCaretPosition("1,00", "2", dfs, 1));
+		assertEquals("get caret", 2, nfv.getNewCaretPosition("1,00", "2", "12,00", dfs, 1, 1));
 	}
 
 	@Test
@@ -88,7 +88,7 @@ public class NumberFieldVerifierTests {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
 
-		assertEquals(4, nfv.getNewCaretPosition("0.00", "9000", dfs, 1));
+		assertEquals(4, nfv.getNewCaretPosition("0.00", "9000", "9,000.00", dfs, 1, 1));
 	}
 
 	@Test
@@ -96,7 +96,7 @@ public class NumberFieldVerifierTests {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
 
-		assertEquals(4, nfv.getNewCaretPosition("0,00", "9000", dfs, 1));
+		assertEquals(4, nfv.getNewCaretPosition("0,00", "9000", "9.000,00", dfs, 1, 1));
 	}
 
 	@Test
@@ -104,7 +104,7 @@ public class NumberFieldVerifierTests {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
 
-		assertEquals(2, nfv.getNewCaretPosition("0.00", ".", dfs, 1));
+		assertEquals(2, nfv.getNewCaretPosition("0.00", ".", "0.00", dfs, 1, 1));
 	}
 
 	@Test
@@ -112,23 +112,41 @@ public class NumberFieldVerifierTests {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
 
-		assertEquals(2, nfv.getNewCaretPosition("0,00", ",", dfs, 1));
+		assertEquals(2, nfv.getNewCaretPosition("0,00", ",", "0,00", dfs, 1, 1));
 	}
-	
-	@Test
-	public void testGetNewCaretPositionInsertCommaUS() {
-		NumberFieldVerifier nfv = new NumberFieldVerifier();
-		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
 
-		assertEquals(3, nfv.getNewCaretPosition("100.00", ",", dfs, 3));
-	}
-	
 	@Test
-	public void testGetNewCaretPositionInsertPointGER() {
+	public void testFrom100_00To1_002_00GER() {
 		NumberFieldVerifier nfv = new NumberFieldVerifier();
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
 
-		assertEquals(3, nfv.getNewCaretPosition("100,00", ".", dfs, 3));
+		assertEquals("get text", "1.002,00", nfv.getNewText(2, Locale.GERMANY, "100,00", 3, 3, 3, "2", dfs));
+		assertEquals("get caret Position", 5, nfv.getNewCaretPosition("100,00", "2", "1.002,00", dfs, 3, 1));
+	}
+
+	@Test
+	public void testFrom100_00To1_002_00US() {
+		NumberFieldVerifier nfv = new NumberFieldVerifier();
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
+
+		assertEquals("get text", "1,002.00", nfv.getNewText(2, Locale.US, "100.00", 3, 3, 3, "2", dfs));
+		assertEquals("get caret Position", 5, nfv.getNewCaretPosition("100.00", "2", "1,002.00", dfs, 3, 1));
+	}
+
+	@Test
+	public void testKeyCode8() {
+		NumberFieldVerifier nfv = new NumberFieldVerifier();
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
+
+		assertEquals("get caret Position", 3, nfv.getNewCaretPosition("1.002,00", "", "100,00", dfs, 4, 8));
+	}
+	
+	@Test
+	public void testKeyCode127() {
+		NumberFieldVerifier nfv = new NumberFieldVerifier();
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.GERMANY);
+
+		assertEquals("get caret Position", 3, nfv.getNewCaretPosition("1.002,00", "", "100,00", dfs, 3, 127));
 	}
 
 }
