e560ca9dd0b1c8f742a9f5c7e7ee73dd7e7a9c38
Christopher Erlanger <erlanger@minova.com>
2020-11-25 13:58:42 +0100

    grenzen für chargededQuantity festlegen

--- bundles/aero.minova.workingtime.helper/src/aero/minova/workingtime/helper/WorkingTimeHelper.java
+++ bundles/aero.minova.workingtime.helper/src/aero/minova/workingtime/helper/WorkingTimeHelper.java
@@ -1,6 +1,5 @@
 package aero.minova.workingtime.helper;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
@@ -14,7 +13,7 @@ import aero.minova.rcp.dataservice.IHelper;
 import aero.minova.rcp.model.Value;
 
 @Component
-public class WorkingTimeHelper implements IHelper{
+public class WorkingTimeHelper implements IHelper {
 
 	Map<String, Control> controls;
 	public static final String CONTROL_CONSUMER = "consumer";
@@ -53,17 +52,31 @@ public class WorkingTimeHelper implements IHelper{
 		Value end = (Value) endDate.getData("value");
 		Instant iStart = start.getInstantValue();
 		Instant iEnd = end.getInstantValue();
-		Duration d = Duration.between(iEnd, iStart);
-		long min = ChronoUnit.MINUTES.between(iEnd, iStart);
+		long min = ChronoUnit.MINUTES.between(iStart, iEnd);
 		getFloatFromMinutes(min);
 
 	}
 
-	public Float getFloatFromMinutes(long min) {
-		Float f = null;
-		f = (float) Math.round(min * 100.0 / 60.0);
+	public float getFloatFromMinutes(long min) {
+		float f = 0.0f;
+		f = Math.round(min * 100.0 / 60.0);
 		f = (float) (f / 100.0);
 		return f;
+	}
+
+	public Float getChagedQunatity(float f) {
+		float chargedQty = f;
+		// z.B. >= 0,75 -> 1
+		// z.B. >= 0,25 -> 0,5
+		int hours = (int) f;
+		float dummy = chargedQty - (hours * 1.0f);
+		chargedQty = hours * 1.0f;
+		if (dummy >= 0.25f && dummy < 0.75f) {
+			chargedQty += 0.5f;
+		} else if (dummy >= 0.75f) {
+			chargedQty += 1.0f;
+		}
+		return chargedQty;
 	}
 
 }
--- tests/aero.minova.rcp.xml.tests/src/aero/minova/rcp/xml/tests/CalculateDateTest.java
+++ tests/aero.minova.rcp.xml.tests/src/aero/minova/rcp/xml/tests/CalculateDateTest.java
@@ -2,8 +2,8 @@ package aero.minova.rcp.xml.tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,16 +15,56 @@ public class CalculateDateTest {
 	private WorkingTimeHelper w;
 
 	@Before
-	private void init() {
+	public void init() {
 		w = new WorkingTimeHelper();
 	}
 	@Test
-	public void testCalculateDuration() {
+	public void testCalculateDuration2Hours() {
 		Instant now = Instant.now();
-		Instant nowPlus2Hours = Instant.now().plusSeconds(720);
-		Duration between = Duration.between(nowPlus2Hours, now);
-		Float f = w.getFloatFromMinutes(between.getSeconds());
-		assertEquals(2.0, f, 0.0);
+		Instant nowPlus2Hours = now.plusSeconds(7200);
+		long min = ChronoUnit.MINUTES.between(now, nowPlus2Hours);
+		Float f = w.getFloatFromMinutes(min);
+		assertEquals(2.00, f, 0.00);
+	}
+
+	@Test
+	public void testCalculateDuration1_15Hours() {
+		Instant now = Instant.now();
+		Instant nowPlus2Hours = now.plusSeconds(4500);
+		long min = ChronoUnit.MINUTES.between(now, nowPlus2Hours);
+		Float f = w.getFloatFromMinutes(min);
+		Float chagedQunatity = w.getChagedQunatity(f);
+		assertEquals(1.5, chagedQunatity, 0.0);
+	}
+
+	@Test
+	public void testCalculateDuration1_14Hours() {
+		Instant now = Instant.now();
+		Instant nowPlus2Hours = now.plusSeconds(4440);
+		long min = ChronoUnit.MINUTES.between(now, nowPlus2Hours);
+		Float f = w.getFloatFromMinutes(min);
+		Float chagedQunatity = w.getChagedQunatity(f);
+		assertEquals(1.0, chagedQunatity, 0.0);
+	}
+
+	@Test
+	public void testCalculateDuration1_44Hours() {
+		Instant now = Instant.now();
+		Instant nowPlus2Hours = now.plusSeconds(6240);
+		long min = ChronoUnit.MINUTES.between(now, nowPlus2Hours);
+		Float f = w.getFloatFromMinutes(min);
+		Float chagedQunatity = w.getChagedQunatity(f);
+		assertEquals(1.5, chagedQunatity, 0.0);
+	}
+
+	@Test
+	public void testCalculateDuration1_45Hours() {
+		Instant now = Instant.now();
+		Instant nowPlus2Hours = now.plusSeconds(6300);
+		long min = ChronoUnit.MINUTES.between(now, nowPlus2Hours);
+		Float f = w.getFloatFromMinutes(min);
+		Float chagedQunatity = w.getChagedQunatity(f);
+		assertEquals(2.0, chagedQunatity, 0.0);
 	}
 
 }
