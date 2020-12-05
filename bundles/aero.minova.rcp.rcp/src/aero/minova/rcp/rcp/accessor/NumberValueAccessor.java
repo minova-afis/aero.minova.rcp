2492ed6b63b4897dbe8494588f5494706a07a1df
Wilfried Saak <saak@minova.de>
2020-12-05 11:28:56 +0100

    Das Ausgabeformat anhand der Einstellungen im XML und Locale erstellen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/accessor/NumberValueAccessor.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/accessor/NumberValueAccessor.java
@@ -1,11 +1,15 @@
 package aero.minova.rcp.rcp.accessor;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
 
 import aero.minova.rcp.model.DataType;
 import aero.minova.rcp.model.Value;
 import aero.minova.rcp.model.form.MField;
+import aero.minova.rcp.rcp.fields.FieldUtil;
 
 public class NumberValueAccessor extends AbstractValueAccessor {
 
@@ -18,10 +22,17 @@ public class NumberValueAccessor extends AbstractValueAccessor {
 		if (value == null) {
 			((Text) control).setText("");
 		} else {
+			int decimals = (int) field.getDecimals();
+			Locale locale = (Locale) control.getData(FieldUtil.TRANSLATE_LOCALE);
+
+			NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+			numberFormat.setMaximumFractionDigits(decimals); // wir wollen genau so viele Nachkommastellen
+			numberFormat.setMinimumFractionDigits(decimals); // dito
+
 			if (value.getType().equals(DataType.DOUBLE)) {
-				((Text) control).setText(value.getDoubleValue().toString());
+				((Text) control).setText(numberFormat.format(value.getDoubleValue()));
 			} else {
-				((Text) control).setText(value.getIntegerValue().toString());
+				((Text) control).setText(numberFormat.format(value.getIntegerValue()));
 			}
 		}
 	}
