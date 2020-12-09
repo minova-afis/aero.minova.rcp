dad2473370d5e3cd11c580296e99374f6d712161
Christopher Erlanger <erlanger@minova.com>
2020-12-09 16:35:01 +0100

    merged

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/parts/WFCIndexPart.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/parts/WFCIndexPart.java
@@ -247,10 +247,6 @@ public class WFCIndexPart extends WFCFormPart {
 
 		// create the body stack
 		bodyLayerStack = new BodyLayerStack<>(table.getRows(), columnPropertyAccessor);
-<<<<<<< Upstream, based on branch 'master' of https://github.com/minova-afis/aero.minova.rcp.git
-=======
-
->>>>>>> 2552d20 Column Accessor korrekt registriert
 		bodyLayerStack.getBodyDataLayer().setConfigLabelAccumulator(new ColumnLabelAccumulator());
 
 		// build the column header layer
