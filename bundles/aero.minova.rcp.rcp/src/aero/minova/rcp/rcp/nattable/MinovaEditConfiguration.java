75598910da6c15436ca0445dd36dc1a7a6b7d831
Christopher Erlanger <erlanger@minova.com>
2020-12-18 10:07:18 +0100

    RF: toten Code entfernen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/MinovaEditConfiguration.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/MinovaEditConfiguration.java
@@ -17,7 +17,6 @@ import org.eclipse.nebula.widgets.nattable.data.convert.DefaultIntegerDisplayCon
 import org.eclipse.nebula.widgets.nattable.edit.EditConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.edit.editor.CheckBoxCellEditor;
 import org.eclipse.nebula.widgets.nattable.edit.editor.EditorSelectionEnum;
-import org.eclipse.nebula.widgets.nattable.edit.editor.TextCellEditor;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ColumnLabelAccumulator;
 import org.eclipse.nebula.widgets.nattable.painter.cell.CheckBoxPainter;
 import org.eclipse.nebula.widgets.nattable.style.CellStyleAttributes;
@@ -33,13 +32,11 @@ public class MinovaEditConfiguration extends AbstractRegistryConfiguration {
 
 	private List<Column> columns;
 	private Locale locale;
-	private TranslationService translationService;
 	private Form form;
 	private Map<String, aero.minova.rcp.form.model.xsd.Column> formColumns;
 
 	public MinovaEditConfiguration(List<Column> columns, TranslationService translationService, Form form) {
 		this.columns = columns;
-		this.translationService = translationService;
 		this.form = form;
 		initFormFields();
 	}
@@ -140,7 +137,6 @@ public class MinovaEditConfiguration extends AbstractRegistryConfiguration {
 
 	private void configureBooleanCell(IConfigRegistry configRegistry, int columnIndex) {
 		// visuelle anpassung [x] oder [_]
-		//
 		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_PAINTER, new CheckBoxPainter(),
 				DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
 
@@ -175,13 +171,6 @@ public class MinovaEditConfiguration extends AbstractRegistryConfiguration {
 
 	}
 
-	private void configureInstantCell(IConfigRegistry configRegistry, int columnIndex) {
-		Style cellStyle = new Style();
-		cellStyle.setAttributeValue(CellStyleAttributes.HORIZONTAL_ALIGNMENT, HorizontalAlignmentEnum.LEFT);
-		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_STYLE, cellStyle, DisplayMode.NORMAL,
-				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-	}
 
 	private void configureTextCell(IConfigRegistry configRegistry, int columnIndex) {
 		MinovaTextCellEditor attributeValue = new MinovaTextCellEditor(true, true);
@@ -194,101 +183,4 @@ public class MinovaEditConfiguration extends AbstractRegistryConfiguration {
 		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_STYLE, cellStyle, DisplayMode.NORMAL,
 				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
 	}
-
-
-	private void registerBooleanEditor(IConfigRegistry configRegistry, int columnIndex) {
-
-		// Das hier würde einen kundenspezifischen Checkbox editor nutzen
-		// configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_PAINTER, new
-		// ExampleCheckBoxPainter(),
-		// DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX +
-		// columnIndex);
-
-		// The CheckBoxCellEditor can also be visualized like a check button
-		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_PAINTER, new CheckBoxPainter(),
-				DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-		// using a CheckBoxCellEditor also needs a Boolean conversion to work
-		// correctly
-		configRegistry.registerConfigAttribute(CellConfigAttributes.DISPLAY_CONVERTER,
-				new DefaultBooleanDisplayConverter(), DisplayMode.NORMAL,
-				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-	}
-
-	private void registerEditors(IConfigRegistry configRegistry) {
-		int i = 0;
-		for (Column column : columns) {
-			
-			if (column.getType().equals(DataType.BOOLEAN)) {
-				registerBooleanEditor(configRegistry, i++);
-			} else if (column.getType().equals(DataType.DOUBLE)) {
-				// auch ein text editor aber mit double konvertierung
-				registerDoubleEditor(configRegistry, i++);
-			}
-			else {
-				registerTextEditor(configRegistry, i++);
-			}
-		}
-	}
-
-	private void registerTextEditor(IConfigRegistry configRegistry, int columnIndex) {
-		// register a TextCellEditor for column two that commits on key up/down
-		// moves the selection after commit by enter
-		TextCellEditor attributeValue = new TextCellEditor(true, true);
-		attributeValue.setSelectionMode(EditorSelectionEnum.START);
-		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, attributeValue,
-				DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-		// configure to open the adjacent editor after commit
-		// default behavior - enter - down, tab - right, shift+tab - left, arrows -
-		// arrow direction
-		configRegistry.registerConfigAttribute(EditConfigAttributes.OPEN_ADJACENT_EDITOR, Boolean.TRUE,
-				DisplayMode.EDIT, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-	}
-
-	private void registerDoubleEditor(IConfigRegistry configRegistry, int columnIndex) {
-		// register a TextCellEditor for column two that commits on key up/down
-		// moves the selection after commit by enter
-		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, new TextCellEditor(true, true),
-				DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-		// configure to open the adjacent editor after commit
-		// default behavior - enter - down, tab - right, shift+tab - left, arrows -
-		// arrow direction
-		configRegistry.registerConfigAttribute(EditConfigAttributes.OPEN_ADJACENT_EDITOR, Boolean.TRUE,
-				DisplayMode.EDIT, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-		// TODO get the number format of the user
-		NumberFormat nf = NumberFormat.getInstance();
-//		NumberFormat nf = NumberFormat.getInstance(new Locale("en", "EN"));
-		DefaultDoubleDisplayConverter defaultDoubleDisplayConverter = new DefaultDoubleDisplayConverter(true);
-		defaultDoubleDisplayConverter.setNumberFormat(nf);
-		configRegistry.registerConfigAttribute(CellConfigAttributes.DISPLAY_CONVERTER,
-				defaultDoubleDisplayConverter, DisplayMode.NORMAL,
-				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-
-
-	}
-
-//	private void registerGenderEditor(IConfigRegistry configRegistry, int columnIndex) {
-//		ComboBoxCellEditor comboBoxCellEditor = new ComboBoxCellEditor(Arrays.asList("Done", "Not Done"));
-//		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, comboBoxCellEditor, DisplayMode.EDIT,
-//				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-//
-//		configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_PAINTER, new ComboBoxPainter(),
-//				DisplayMode.NORMAL, ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-//	}
-
-
-//	private void registerDateEditor(IConfigRegistry configRegistry, int columnIndex) {
-//		DateCellEditor dateCellEditor = new DateCellEditor();
-//		configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, dateCellEditor, DisplayMode.EDIT,
-//				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-//
-//		// using a DateCellEditor also needs a Date conversion to work correctly
-//		configRegistry.registerConfigAttribute(CellConfigAttributes.DISPLAY_CONVERTER,
-//				new DefaultLocalDateDisplayConverter(), DisplayMode.NORMAL,
-//				ColumnLabelAccumulator.COLUMN_LABEL_PREFIX + columnIndex);
-//	}
-
 }
