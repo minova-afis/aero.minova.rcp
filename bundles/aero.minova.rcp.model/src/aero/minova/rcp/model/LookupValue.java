58340e3ecc4a6b7b59705440c867bed063df17de
Wilfried Saak <saak@minova.aero>
2021-02-06 12:28:39 +0100

    LookupValue als Value vom Typ Integer definieren

--- bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/LookupValue.java
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/LookupValue.java
@@ -1,11 +1,13 @@
 package aero.minova.rcp.model;
 
-public class LookupValue {
-	public final int keyLong;
+public class LookupValue extends Value {
+	private static final long serialVersionUID = 202102061225L;
+	public final Integer keyLong;
 	public final String keyText;
 	public final String description;
 
-	public LookupValue(int keyLong, String keyText, String description) {
+	public LookupValue(Integer keyLong, String keyText, String description) {
+		super(keyLong);
 		this.keyLong = keyLong;
 		this.keyText = keyText == null ? "" : keyText;
 		this.description = description == null ? "" : description;
