7c4a788d6d5f56b4aa4729ea3c2badc083e95861
Christopher Erlanger <erlanger@minova.com>
2021-03-12 17:46:07 +0100

    Debugmeldungen für das Tracing

--- bundles/aero.minova.workingtime.helper/src/aero/minova/workingtime/helper/TicketHelper.java
+++ bundles/aero.minova.workingtime.helper/src/aero/minova/workingtime/helper/TicketHelper.java
@@ -1,5 +1,7 @@
 package aero.minova.workingtime.helper;
 
+import org.eclipse.core.runtime.Platform;
+
 import aero.minova.rcp.model.DataType;
 import aero.minova.rcp.model.Value;
 import aero.minova.rcp.model.event.ValueChangeEvent;
@@ -8,6 +10,7 @@ import aero.minova.rcp.model.form.MLookupField;
 
 public class TicketHelper implements ValueChangeListener {
 
+	private static final boolean LOG = "true".equalsIgnoreCase(Platform.getDebugOption("aero.minova.workingtime.heper/debug/tickethelper"));
 	WorkingTimeHelper workingTimeHelper;
 
 	public TicketHelper(WorkingTimeHelper workingTimeHelper) {
@@ -19,7 +22,9 @@ public class TicketHelper implements ValueChangeListener {
 		MLookupField lookupField = (MLookupField) evt.getField();
 		String writtenText = lookupField.getWrittenText();
 		if (writtenText != null && writtenText.startsWith("#")) {
-			// System.out.println("gelesener Text aus dem Lookup:" + writtenText);
+			if (LOG) {
+				System.out.println("gelesener Text aus dem Lookup:" + writtenText);
+			}
 			lookupField.setWrittenText(null);
 			Value value = new Value(writtenText.replace("#", ""), DataType.STRING);
 			workingTimeHelper.postEvent(value);
