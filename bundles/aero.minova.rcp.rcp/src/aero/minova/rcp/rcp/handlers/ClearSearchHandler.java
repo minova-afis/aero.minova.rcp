459b808e137f842eb85d1200f94c23417782c0ae
Christopher Erlanger <erlanger@minova.com>
2021-03-18 11:34:38 +0100

    Löschen von Zeilen aus dem Suchbereich

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClearSearchHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/ClearSearchHandler.java
@@ -5,14 +5,18 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
+import aero.minova.rcp.constants.Constants;
 import aero.minova.rcp.core.ui.PartsID;
-import aero.minova.rcp.model.Table;
 
 public class ClearSearchHandler {
+
+	@Inject
+	IEventBroker broker;
 
 	@Inject
 	EModelService model;
@@ -20,9 +24,6 @@ public class ClearSearchHandler {
 	@Execute
 	public void execute(MPerspective mPerspective) {
 		List<MPart> findElements = model.findElements(mPerspective, PartsID.SEARCH_PART, MPart.class);
-		Table table = (Table) findElements.get(0).getContext().get("NatTableDataSearchArea");
-		table.getRows().clear();
-		table.addRow();
+		broker.post(Constants.BROKER_DELETEROWSEARCHTABLE, PartsID.SEARCH_PART);
 	}
-
 }
