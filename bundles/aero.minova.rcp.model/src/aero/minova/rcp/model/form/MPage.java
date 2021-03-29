1003d990aa5c2efdcfff8b5192b5b114b752f174
David Bauer <bauer@minova.com>
2021-03-29 14:08:31 +0200

    MPage Klasse erstellen

---
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/form/MPage.java
@@ -0,0 +1,83 @@
+package aero.minova.rcp.model.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+
+public class MPage {
+
+	// nur für die 1. Page möglich. Befindet sich immer an oberster Stelle
+	private boolean isHead;
+	// Zustand der Page (invisible, open, closed, minimized)
+	private String status;
+	// Das übergeordnete Element, welches alle Sections enthält
+	private MDetail mDetail;
+	// Liste an allen mit Tab erreichbaren Feldern der Section. Festlegen dieser
+	// anhand der Preferences
+	private List<MField> tabList = new ArrayList<MField>();
+	// Text für diese Page
+	private String label;
+	// Symbol für diese Page
+	private Image icon;
+
+	public MPage(boolean isHead, String status, MDetail mDetail, String label) {
+		this.isHead = isHead;
+		this.status = status;
+		this.mDetail = mDetail;
+		this.label = label;
+	}
+
+	public boolean isHead() {
+		return isHead;
+	}
+
+	public void setHead(boolean isHead) {
+		this.isHead = isHead;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public MDetail getmDetail() {
+		return mDetail;
+	}
+
+	public void setmDetail(MDetail mDetail) {
+		this.mDetail = mDetail;
+	}
+
+	public List<MField> getTabList() {
+		return tabList;
+	}
+
+	public void setTabList(List<MField> tabList) {
+		this.tabList = tabList;
+	}
+
+	public void addTabField(MField mField) {
+		this.tabList.add(mField);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Image getIcon() {
+		return icon;
+	}
+
+	public void setIcon(Image icon) {
+		this.icon = icon;
+	}
+
+}
