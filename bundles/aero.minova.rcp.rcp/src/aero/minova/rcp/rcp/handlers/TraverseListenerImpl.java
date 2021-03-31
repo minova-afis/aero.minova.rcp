c9c51d92ea66934792e156ab79d7c75d5486e468
David Bauer <bauer@minova.com>
2021-03-31 13:04:55 +0200

    TraverseListener erstellen

---
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/TraverseListenerImpl.java
@@ -0,0 +1,175 @@
+package aero.minova.rcp.rcp.handlers;
+
+import java.util.List;
+
+import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.nebula.widgets.opal.textassist.TextAssist;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.widgets.Control;
+
+import aero.minova.rcp.model.form.MDetail;
+import aero.minova.rcp.model.form.MField;
+import aero.minova.rcp.model.form.MPage;
+import aero.minova.rcp.rcp.accessor.AbstractValueAccessor;
+import aero.minova.rcp.rcp.widgets.Lookup;
+
+public class TraverseListenerImpl implements TraverseListener {
+
+	Logger logger;
+	MDetail detail;
+
+	public TraverseListenerImpl(Logger logger, MDetail detail) {
+		this.logger = logger;
+		this.detail = detail;
+	}
+
+	@Override
+	public void keyTraversed(TraverseEvent e) {
+
+		logger.info("keyTraversed(detail=" + e.detail + ", stateMask=" + Integer.toHexString(e.stateMask)
+				+ ", keyCode= " + Integer.toHexString(e.keyCode) + ")");
+		List<MPage> pageList;
+		// if (!e.doit) return; // wir tun nichts, wenn ein anderer etwas getan hat
+
+		Control focussedControl = (Control) e.widget;
+		if (focussedControl.getParent() instanceof Lookup) {
+			focussedControl = focussedControl.getParent();
+		} else if (focussedControl.getParent() instanceof TextAssist) {
+			focussedControl = focussedControl.getParent();
+		}
+
+		switch (e.detail) {
+		case SWT.TRAVERSE_ARROW_NEXT:
+			logger.info("SWT.TRAVERSE_ARROW_NEXT");
+			break;
+		case SWT.TRAVERSE_ARROW_PREVIOUS:
+			logger.info("SWT.TRAVERSE_ARROW_PREVIOUS");
+			break;
+		case SWT.TRAVERSE_ESCAPE:
+			logger.info("SWT.TRAVERSE_ESCAPE");
+			break;
+		case SWT.TRAVERSE_MNEMONIC:
+			logger.info("SWT.TRAVERSE_MNEMONIC");
+			break;
+		case SWT.TRAVERSE_NONE:
+			logger.info("SWT.TRAVERSE_NONE");
+			break;
+		case SWT.TRAVERSE_PAGE_NEXT:
+			logger.info("SWT.TRAVERSE_PAGE_NEXT");
+			break;
+		case SWT.TRAVERSE_PAGE_PREVIOUS:
+			logger.info("SWT.TRAVERSE_PAGE_PREVIOUS");
+			break;
+		case SWT.TRAVERSE_RETURN:
+			logger.info("SWT.TRAVERSE_RETURN");
+			break;
+		case SWT.TRAVERSE_TAB_NEXT:
+			logger.info("SWT.TRAVERSE_TAB_NEXT");
+			if (e.keyCode == SWT.TAB) {
+				getNextField(focussedControl);
+			} else if (e.keyCode == SWT.CR) {
+				getNextRequired(focussedControl);
+			}
+			e.doit = false;
+			break;
+		case SWT.TRAVERSE_TAB_PREVIOUS:
+			logger.info("SWT.TRAVERSE_TAB_PREVIOUS");
+			getPreviousField(focussedControl);
+			e.doit = false;
+			break;
+		default:
+			logger.info("UNNOWN");
+		}
+
+	}
+
+	private void getPreviousField(Control focussedControl) {
+		List<MPage> pageList = detail.getPageList();
+		for (MPage page : pageList) {
+			List<MField> tabList = page.getTabList();
+			for (MField f : tabList) {
+				if (((AbstractValueAccessor) f.getValueAccessor()).getControl() == focussedControl) {
+					if (tabList.indexOf(f) == 0) {
+						// Wir sind an der ersten Stelle der Section
+						if (pageList.indexOf(page) == 0) {
+							// Wir sind in der ersten Section
+							List<MField> lastTabList = pageList.get(pageList.size() - 1).getTabList();
+							focussedControl = ((AbstractValueAccessor) lastTabList.get(lastTabList.size() - 1)
+									.getValueAccessor()).getControl();
+						} else {
+							List<MField> previousTabList = pageList.get(pageList.indexOf(page) - 1).getTabList();
+							focussedControl = ((AbstractValueAccessor) previousTabList.get(previousTabList.size() - 1)
+									.getValueAccessor()).getControl();
+						}
+					} else {
+						focussedControl = ((AbstractValueAccessor) tabList.get(tabList.indexOf(f) - 1)
+								.getValueAccessor()).getControl();
+					}
+					focussedControl.setFocus();
+					return;
+				}
+			}
+		}
+
+	}
+
+	private void getNextField(Control focussedControl) {
+		List<MPage> pageList = detail.getPageList();
+		for (MPage page : pageList) {
+			List<MField> tabList = page.getTabList();
+			for (MField f : tabList) {
+				if (((AbstractValueAccessor) f.getValueAccessor()).getControl() == focussedControl) {
+					if (tabList.indexOf(f) == tabList.size() - 1) {
+						// Wir sind an der Letzen Stelle der Section
+						if (pageList.indexOf(page) == pageList.size() - 1) {
+							// Wir sind in der Letzten Section
+							focussedControl = ((AbstractValueAccessor) pageList.get(0).getTabList().get(0)
+									.getValueAccessor()).getControl();
+						} else {
+							focussedControl = ((AbstractValueAccessor) pageList.get(pageList.indexOf(page) + 1)
+									.getTabList().get(0).getValueAccessor()).getControl();
+						}
+					} else {
+						focussedControl = ((AbstractValueAccessor) tabList.get(tabList.indexOf(f) + 1)
+								.getValueAccessor()).getControl();
+					}
+					focussedControl.setFocus();
+					return;
+				}
+			}
+		}
+
+	}
+
+	private void getNextRequired(Control focussedControl) {
+		MField selectedField = null;
+		List<MPage> pageList = detail.getPageList();
+		for (MPage page : pageList) {
+			List<MField> tabList = page.getTabList();
+			for (MField field : tabList) {
+				if (((AbstractValueAccessor) field.getValueAccessor()).getControl() == focussedControl) {
+					selectedField = field;
+				}
+			}
+		}
+		for (MPage page : pageList) {
+			if (pageList.indexOf(page) >= pageList.indexOf(focussedControl)) {
+				List<MField> tabList = page.getTabList();
+				for (MField field : tabList) {
+					if ((selectedField.getmPage() == page && tabList.indexOf(field) > tabList.indexOf(selectedField))
+							|| (pageList.indexOf(selectedField.getmPage()) < pageList.indexOf(page))) {
+						if (field.isRequired() == true) {
+							focussedControl = ((AbstractValueAccessor) field.getValueAccessor()).getControl();
+							focussedControl.setFocus();
+							return;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+}
