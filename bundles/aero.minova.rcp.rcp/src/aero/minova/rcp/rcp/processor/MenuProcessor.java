6aeea35ada1d469d981d3bf846144ed7ed5a7c25
Lea Janiak <janiak@minova.aero>
2021-05-14 10:20:06 +0200

    Bei Problem mit Download von application.mdi Fehler anzeigen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/processor/MenuProcessor.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/processor/MenuProcessor.java
@@ -1,5 +1,6 @@
 package aero.minova.rcp.rcp.processor;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -8,13 +9,15 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.xml.bind.JAXBException;
 
-import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.commands.MCommand;
 import org.eclipse.e4.ui.model.application.commands.MParameter;
 import org.eclipse.e4.ui.model.application.ui.menu.MHandledMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 import aero.minova.rcp.constants.Constants;
 import aero.minova.rcp.dataservice.IDataService;
@@ -32,8 +35,8 @@ public class MenuProcessor {
 	private EModelService modelService;
 	private MApplication mApplication;
 
-	@Inject
-	private IEventBroker broker;
+	private String usingLocalMenu = "There was no response from the server. Application data may be out of date. Consider checking your connection and restarting the application.";
+	private String couldntLoadMenu = "There was no response from the server. The application isn't loaded properly. Please check your connection and restart the application.";
 
 	@Inject
 	public MenuProcessor(@Named("org.eclipse.ui.main.menu") MMenu menu, EModelService modelService, IDataService dataService, MApplication mApplication) {
@@ -45,13 +48,12 @@ public class MenuProcessor {
 		try {
 			dataService.getHashedFile(MDI_FILE_NAME).thenAccept(this::processXML);
 		} catch (Exception e) {
-			// Datei/Hash für Datei konnte nicht vom Server geladen werden, Versuchen lokale Datei zu nutzen
-			try {
-				processXML(dataService.getCachedFileContent(MDI_FILE_NAME).get());
-				broker.post(Constants.BROKER_SHOWCONNECTIONERRORMESSAGE, "msg.WFCUsingLocalMenu");
-			} catch (InterruptedException | ExecutionException e1) {
-				broker.post(Constants.BROKER_SHOWCONNECTIONERRORMESSAGE, "msg.WFCCouldntLoadMenu");
-			}
+			handleNoMDI(dataService);
+		}
+
+		File application = new File(dataService.getStoragePath() + "/" + MDI_FILE_NAME);
+		if (!application.exists()) {
+			handleNoMDI(dataService);
 		}
 	}
 
@@ -158,7 +160,21 @@ public class MenuProcessor {
 		handledMenuItem.setIconURI(actionMDI.getIcon());
 
 		return handledMenuItem;
+	}
 
+	private void handleNoMDI(IDataService dataService) {
+		// Datei/Hash für Datei konnte nicht vom Server geladen werden, Versuchen lokale Datei zu nutzen
+		try {
+			processXML(dataService.getCachedFileContent(MDI_FILE_NAME).get());
+			showConnectionErrorMessage(usingLocalMenu);
+		} catch (InterruptedException | ExecutionException e1) {
+			showConnectionErrorMessage(couldntLoadMenu);
+		}
+	}
+
+	public void showConnectionErrorMessage(String message) {
+		Shell shell = new Shell(Display.getCurrent());
+		MessageDialog.openError(shell, "Error", message);
 	}
 
 }
