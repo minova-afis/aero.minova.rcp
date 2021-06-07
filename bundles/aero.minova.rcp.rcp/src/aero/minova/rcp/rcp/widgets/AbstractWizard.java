66a36d67d67f9011191ccbd5cd99e121cfe76771
Lea Janiak <janiak@minova.aero>
2021-06-07 11:04:24 +0200

    Injection direkt im eigentlichen Wizard durchführen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/widgets/AbstractWizard.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/widgets/AbstractWizard.java
@@ -1,28 +1,10 @@
 package aero.minova.rcp.rcp.widgets;
 
-import javax.inject.Inject;
-
-import org.eclipse.e4.core.services.translation.TranslationService;
-import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.jface.wizard.Wizard;
 
-import aero.minova.rcp.dataservice.IDataService;
 import aero.minova.rcp.model.form.MDetail;
 
 public abstract class AbstractWizard extends Wizard {
-
-	@Inject
-	protected MPerspective mPerspective;
-
-	@Inject
-	protected TranslationService translationService;
-
-	@Inject
-	protected MPart mPart;
-
-	@Inject
-	protected IDataService dataService;
 
 	protected MDetail originalMDetail;
 
