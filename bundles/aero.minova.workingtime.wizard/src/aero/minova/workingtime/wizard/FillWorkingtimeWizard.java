0d68894f99cee0d05a2e5f291471a18751354304
Lea Janiak <janiak@minova.aero>
2021-06-07 11:05:36 +0200

    Index gegebenenfalls automatisch neu laden

--- bundles/aero.minova.workingtime.wizard/src/aero/minova/workingtime/wizard/FillWorkingtimeWizard.java
+++ bundles/aero.minova.workingtime.wizard/src/aero/minova/workingtime/wizard/FillWorkingtimeWizard.java
@@ -2,17 +2,44 @@ package aero.minova.workingtime.wizard;
 
 import java.util.concurrent.CompletableFuture;
 
+import javax.inject.Inject;
+
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.core.di.extensions.Preference;
+import org.eclipse.e4.core.services.translation.TranslationService;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 
+import aero.minova.rcp.dataservice.IDataService;
 import aero.minova.rcp.dialogs.NotificationPopUp;
 import aero.minova.rcp.model.SqlProcedureResult;
 import aero.minova.rcp.model.Table;
+import aero.minova.rcp.preferences.ApplicationPreferences;
 
 /**
  * Mit diesem Assistent (Wizard) kann man Arbeitszeiten über mehrere Tage auffüllen
  */
 public class FillWorkingtimeWizard extends MinovaWizard {
+
+	@Inject
+	protected TranslationService translationService;
+	@Inject
+	protected MPerspective mPerspective;
+	@Inject
+	protected MPart mPart;
+	@Inject
+	protected IDataService dataService;
+	@Inject
+	@Preference(nodePath = ApplicationPreferences.PREFERENCES_NODE, value = ApplicationPreferences.AUTO_LOAD_INDEX)
+	boolean autoLoadIndex;
+	@Inject
+	private ECommandService commandService;
+	@Inject
+	private EHandlerService handlerService;
 
 	public FillWorkingtimeWizard() {
 		super("@Workingtime.FillWizard.Name");
@@ -55,6 +82,11 @@ public class FillWorkingtimeWizard extends MinovaWizard {
 					NotificationPopUp notificationPopUp = new NotificationPopUp(Display.getCurrent(), translationService.translate("@msg.DataUpdated", null),
 							Display.getCurrent().getActiveShell());
 					notificationPopUp.open();
+
+					if (autoLoadIndex) {
+						ParameterizedCommand cmd = commandService.createCommand("aero.minova.rcp.rcp.command.loadindex", null);
+						handlerService.executeHandler(cmd);
+					}
 				});
 			}
 		});
