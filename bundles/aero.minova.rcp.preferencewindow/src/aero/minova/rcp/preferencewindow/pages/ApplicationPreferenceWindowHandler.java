9349eb3200698dbb3442ef22f5ea0dd5ef571d6b
Bauer-David <bauer@minova.com>
2021-06-24 14:05:56 +0200

    Neustart Meldung in den Preferences anpassen an die TabListen Änderung

--- bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/pages/ApplicationPreferenceWindowHandler.java
+++ bundles/aero.minova.rcp.preferencewindow/src/aero/minova/rcp/preferencewindow/pages/ApplicationPreferenceWindowHandler.java
@@ -106,7 +106,8 @@ public class ApplicationPreferenceWindowHandler {
 		shell.setEnabled(false);
 
 		String currentTheme = (String) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.FONT_SIZE, DisplayType.COMBO, "M", s);
-		boolean curentSelectAllControls = (boolean) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.SELECT_ALL_CONTROLS, DisplayType.CHECK, true, s);
+		boolean curentSelectAllControls = (boolean) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.SELECT_ALL_CONTROLS,
+				DisplayType.CHECK, true, s);
 		List<PreferenceTabDescriptor> preferenceTabs = pwm.createModel(translationService);
 		Map<String, Object> data = fillData(preferenceTabs);
 		PreferenceWindow window = PreferenceWindow.create(shell, data);
@@ -168,22 +169,19 @@ public class ApplicationPreferenceWindowHandler {
 			// Preference Handler wieder aktivieren
 			handlerService.activateHandler("org.eclipse.ui.window.preferences", preferenceHandler.getObject());
 		}
-		
-		boolean newSelectAllControls = (boolean) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.SELECT_ALL_CONTROLS, DisplayType.CHECK, true, s);
-		if(!curentSelectAllControls == newSelectAllControls) {
-			Shell activeShell = Display.getCurrent().getActiveShell();
-			boolean openConfirm = MessageDialog.openConfirm(activeShell, "Neustart", "Soll die Tab-Reihenfolge geändert werden und die Applikation neu gestarted werden");
-			if (openConfirm) {
-				workbench.restart();
-			}
-		}
 
+		boolean newSelectAllControls = (boolean) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.SELECT_ALL_CONTROLS, DisplayType.CHECK,
+				true, s);
 		String newTheme = (String) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.FONT_SIZE, DisplayType.COMBO, "M", s);
-		if (!currentTheme.equals(newTheme) ) {
+		if (!currentTheme.equals(newTheme) || !curentSelectAllControls == newSelectAllControls) {
 			Shell activeShell = Display.getCurrent().getActiveShell();
-			boolean openConfirm = MessageDialog.openConfirm(activeShell, "Neustart", "Soll das Theme geändert werden und die Applikation neu gestarted werden");
+			boolean openConfirm = MessageDialog.openConfirm(activeShell, "Neustart", "Zum übernehmen der Änderungen, muss die Applikation neu gestarted werden");
 			if (openConfirm) {
-				updateTheme(newTheme, themeEngine, workbench);
+				if (!currentTheme.equals(newTheme)) {
+					updateTheme(newTheme, themeEngine, workbench);
+				} else {
+					workbench.restart();
+				}
 			}
 		}
 
