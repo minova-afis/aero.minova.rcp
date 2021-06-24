96239af2d9c7e2c5a6d75e97f411a18386019b42
Lea Janiak <janiak@minova.aero>
2021-06-24 15:13:37 +0200

    Überprüfen, ob Menüeinträge richtig angeordnet sind

--- tests/aero.minova.rcp.uitests/src/aero/minova/rcp/uitests/MenuTest.java
+++ tests/aero.minova.rcp.uitests/src/aero/minova/rcp/uitests/MenuTest.java
@@ -1,21 +1,27 @@
 package aero.minova.rcp.uitests;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.e4.core.commands.ECommandService;
 import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.translation.TranslationService;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.e4.finder.widgets.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotRootMenu;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
 import org.junit.Assert;
@@ -27,16 +33,21 @@ import org.osgi.framework.FrameworkUtil;
 
 import aero.minova.rcp.dataservice.XmlProcessor;
 import aero.minova.rcp.form.menu.mdi.Main;
+import aero.minova.rcp.form.menu.mdi.Main.Action;
+import aero.minova.rcp.form.menu.mdi.Main.Entry;
+import aero.minova.rcp.form.menu.mdi.MenuType;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class MenuTest {
 
 	private static SWTWorkbenchBot bot;
+	TranslationService translationService;
 
 	@Before
 	public void beforeClass() throws Exception {
 		bot = new SWTWorkbenchBot(getEclipseContext());
 		SWTBotPreferences.TIMEOUT = 30000;
+		this.translationService = getEclipseContext().get(TranslationService.class);
 	}
 
 	@Test
@@ -67,10 +78,52 @@ public class MenuTest {
 			assertNotNull(applicationString);
 			Main mainMDI = XmlProcessor.get(applicationString, Main.class);
 			assertNotNull(mainMDI);
+
+			// Liste mit Menueinträgen (depth-first)
+			List<String> menuEntries = new ArrayList<>();
+			SWTBotRootMenu menu = bot.menu();
+			for (String menuEntry : menu.menuItems()) {
+				SWTBotMenu menu2 = bot.menu(menuEntry);
+				getMenuEntries(menuEntries, menu2);
+			}
+
+			// TODO Reihenfolge in der mdi beachten (siehe MenuProcessor)
+			int counter = 0;
+			for (Object menuOrEntry : mainMDI.getMenu().getMenuOrEntry()) {
+				counter = checkEntries(menuEntries, counter, menuOrEntry);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.getMessage());
 		}
+	}
+
+	private void getMenuEntries(List<String> entries, SWTBotMenu menu) {
+		entries.add(menu.getText());
+		for (String menuEntry : menu.menuItems()) {
+			getMenuEntries(entries, menu.menu(menuEntry));
+		}
+	}
+
+	private int checkEntries(List<String> entries, int counter, Object menuOrEntry) {
+		if (menuOrEntry instanceof MenuType) {
+			String translated = translationService.translate(((MenuType) menuOrEntry).getText(), null);
+			assertEquals(translated, entries.get(counter));
+			counter++;
+
+			for (Object o : ((MenuType) menuOrEntry).getEntryOrMenu()) {
+				counter = checkEntries(entries, counter, o);
+			}
+		} else {
+			Entry entry = (Entry) menuOrEntry;
+			if (entry.getId() instanceof Action) {
+				String name = ((Action) entry.getId()).getText();
+				String translated = translationService.translate(name, null);
+				assertEquals(translated, entries.get(counter));
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	protected static IEclipseContext getEclipseContext() {
