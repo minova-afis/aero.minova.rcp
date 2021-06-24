64eae1fdd2dc76666b4b75e2a7c951b338905f3a
Lea Janiak <janiak@minova.aero>
2021-06-24 12:08:27 +0200

    Einstellungen über Command öffnen (Platformunabhängig)

--- tests/aero.minova.rcp.uitests/src/aero/minova/rcp/uitests/OpenStundenerfassungsTest.java
+++ tests/aero.minova.rcp.uitests/src/aero/minova/rcp/uitests/OpenStundenerfassungsTest.java
@@ -1,6 +1,5 @@
 package aero.minova.rcp.uitests;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -8,27 +7,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.workbench.IWorkbench;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swtbot.e4.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.e4.finder.widgets.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarButton;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
@@ -61,7 +57,7 @@ public class OpenStundenerfassungsTest {
 		assertNotNull(partByTitle);
 
 		List<SWTBotToolbarButton> toolbarButtons = partByTitle.getToolbarButtons();
-		assertTrue(toolbarButtons.size() > 0);
+		assertTrue(!toolbarButtons.isEmpty());
 		toolbarButtons.get(0).click();
 
 //		SWTNatTableBot swtNatTableBot = new SWTNatTableBot();
@@ -85,26 +81,18 @@ public class OpenStundenerfassungsTest {
 
 	}
 
-	@Ignore
 	@Test
-	public void openPreferences() {
+	public void openPreferencesAndTestMenu() {
 
-		if (!SWTUtils.isMac()) {
-			return;
-		}
-
+		// Einstellungen über Command öffnen
 		Display.getDefault().asyncExec(() -> {
-			Menu systemMenu = Display.getDefault().getSystemMenu();
-			MenuItem menuItem = systemMenu.getItem(2);
-			assertEquals(menuItem.toString(), "MenuItem {Preferences...}");
-			Event event = new Event();
-			event.widget = menuItem;
-			event.display = Display.getDefault();
-			menuItem.setSelection(true);
-			menuItem.notifyListeners(SWT.Selection, event);
-
+			ECommandService commandService = getEclipseContext().get(ECommandService.class);
+			EHandlerService handlerService = getEclipseContext().get(EHandlerService.class);
+			ParameterizedCommand cmd = commandService.createCommand("org.eclipse.ui.window.preferences", null);
+			handlerService.executeHandler(cmd);
 		});
 
+		// Workspace-Ordner auslesen
 		SWTBotShell shell = bot.shell("Preferences");
 		assertNotNull(shell);
 		SWTBot childBot = new SWTBot(shell.widget);
@@ -121,7 +109,6 @@ public class OpenStundenerfassungsTest {
 			assertNotNull(applicationString);
 			Main mainMDI = XmlProcessor.get(applicationString, Main.class);
 			assertNotNull(mainMDI);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.getMessage());
