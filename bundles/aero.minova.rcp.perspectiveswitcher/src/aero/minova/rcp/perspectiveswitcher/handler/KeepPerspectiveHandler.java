8912646a820caec75a1fed97c649195a24217459
Bauer-David <bauer@minova.com>
2021-07-01 14:40:06 +0200

    KeepPerspective um alle nötigen Perspective Daten erweitert

--- bundles/aero.minova.rcp.perspectiveswitcher/src/aero/minova/rcp/perspectiveswitcher/handler/KeepPerspectiveHandler.java
+++ bundles/aero.minova.rcp.perspectiveswitcher/src/aero/minova/rcp/perspectiveswitcher/handler/KeepPerspectiveHandler.java
@@ -1,5 +1,7 @@
 package aero.minova.rcp.perspectiveswitcher.handler;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -7,6 +9,7 @@ import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
@@ -41,17 +44,28 @@ public class KeepPerspectiveHandler {
 	
 	Preferences prefs = InstanceScope.INSTANCE.getNode(Constants.PREFERENCES_KEPTPERSPECTIVES);
 
-	@SuppressWarnings("unchecked")
 	@Execute
 	public void execute(MWindow window, @Optional @Named(E4WorkbenchParameterConstants.FORM_NAME) String perspectiveId) {
 
+		List<MPerspective> perspectives = modelService.findElements(window, perspectiveId, MPerspective.class);
+		MPerspective perspective = perspectives.get(0);
 
-		String keptPerspective = prefs.get(perspectiveId, "");
+		String keptPerspective = prefs.get(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMNAME, "");
 
 		if (keptPerspective.isBlank()) {
-			prefs.put(perspectiveId, perspectiveId);
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMNAME, perspective.getPersistedState().get(Constants.FORM_NAME));
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMID, perspective.getElementId());
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMLABEL, perspective.getLabel());
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_ICONURI, perspective.getIconURI());
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_LOCALIZEDLABEL, perspective.getLocalizedLabel());
+			prefs.put(perspectiveId + Constants.KEPT_PERSPECTIVE_LOCALIZEDTOOLTIP, perspective.getLocalizedTooltip());
 		} else {
-			prefs.remove(perspectiveId);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMNAME);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMID);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_FORMLABEL);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_ICONURI);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_LOCALIZEDLABEL);
+			prefs.remove(perspectiveId + Constants.KEPT_PERSPECTIVE_LOCALIZEDTOOLTIP);
 		}
 		
 		try {
