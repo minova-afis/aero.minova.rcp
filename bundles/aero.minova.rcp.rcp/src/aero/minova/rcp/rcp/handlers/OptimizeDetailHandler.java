edf9d519a59218bf2b909a0a5c6dd23796ea98a0
Lea Janiak <janiak@minova.aero>
2021-07-23 14:52:08 +0200

    Nicht benutze Handler/Commands löschen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeDetailHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeDetailHandler.java
@@ -3,6 +3,7 @@ package aero.minova.rcp.rcp.handlers;
 import java.util.List;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainerElement;
@@ -12,11 +13,10 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 public class OptimizeDetailHandler {
 
 	@Execute
-	public void execute(EModelService emservice, MWindow mwindow) {
-		MPartSashContainer element = (MPartSashContainer) emservice.find("aero.minova.rcp.rcp.partsashcontainer.main",
-				mwindow);
+	public void execute(EModelService emservice, MWindow mwindow, MPerspective perspective) {
+		MPartSashContainer element = (MPartSashContainer) emservice.find("aero.minova.rcp.rcp.partsashcontainer.main", mwindow);
 		element.getChildren();
-		
+
 		List<MPart> partStack = emservice.findElements(emservice.getActivePerspective(mwindow), "aero.minova.rcp.rcp.part.details", MPart.class);
 		MPart detail = partStack.get(0);
 
@@ -32,12 +32,11 @@ public class OptimizeDetailHandler {
 		float dummy = (float) ((detailWidth + 25.0) / mwindow.getWidth());
 		float dummy2 = 1.0f - dummy;
 
-
 		for (MPartSashContainerElement e : element.getChildren()) {
 			if (e.getElementId().equalsIgnoreCase("aero.minova.rcp.rcp.partstack.details")) {
 				e.setContainerData(Integer.toString((int) (dummy * size)));
 			} else {
-				e.setContainerData(Integer.toString((int)(dummy2 * size)));
+				e.setContainerData(Integer.toString((int) (dummy2 * size)));
 			}
 		}
 	}
