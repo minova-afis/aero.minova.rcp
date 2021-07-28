26a82454f320aaa2fba1b16ca8c6b5624d90cc5f
Lars Vogel <Lars.Vogel@vogella.com>
2021-07-28 10:30:10 +0200

    Füge Service zum herunterladen des Plugins Folders hinzu

---
+++ bundles/aero.minova.rcp.dataservice/src/aero/minova/rcp/dataservice/internal/PluginDownloadService.java
@@ -0,0 +1,26 @@
+package aero.minova.rcp.dataservice.internal;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import aero.minova.rcp.dataservice.IDataService;
+import aero.minova.rcp.dataservice.IDummyService;
+
+@Component(immediate = true)
+public class PluginDownloadService {
+
+	private IDataService dataService;
+
+	@Reference
+	void getDummyService(IDummyService dummyService) {
+		// this method prevents the immediate component to be actived before the
+		// workspace location in the dataservice has been set
+		// maybe move that to a service property dependency?
+	}
+
+	@Reference
+	void getDataService(IDataService dataService) {
+		this.dataService = dataService;
+		dataService.getHashedZip("plugins.zip");
+	}
+}
