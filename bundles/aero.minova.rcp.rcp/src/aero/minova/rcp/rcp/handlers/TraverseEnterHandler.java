0b19f3f84e5b05a3c691410de9a6a63f112622f0
Bauer-David <bauer@minova.com>
2021-08-10 11:09:09 +0200

    RF: Code bereinigen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/TraverseEnterHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/TraverseEnterHandler.java
@@ -29,7 +29,6 @@ import org.osgi.service.prefs.Preferences;
 
 import aero.minova.rcp.constants.Constants;
 import aero.minova.rcp.model.LookupValue;
-import aero.minova.rcp.model.Table;
 import aero.minova.rcp.model.form.MDetail;
 import aero.minova.rcp.model.form.MField;
 import aero.minova.rcp.preferences.ApplicationPreferences;
@@ -58,8 +57,6 @@ public class TraverseEnterHandler {
 
 	@Inject
 	EHandlerService handlerService;
-
-	MField selectedField;
 
 	@Execute
 	public void execute() {
@@ -115,7 +112,6 @@ public class TraverseEnterHandler {
 				// Wir holen uns den Status des Popup des Lookup
 				popupOpen = lookup.popupIsOpen();
 			}
-			selectedField = (MField) focussedControl.getData(Constants.CONTROL_FIELD);
 		} else {
 			focussedControl = control;
 		}
@@ -226,195 +222,6 @@ public class TraverseEnterHandler {
 		return tabList;
 	}
 
-//	/**
-//	 * Ermittelt das in der Tab Reihenfolge nachfolgende Pflichtfeld und selektiert dieses.
-//	 * <p>
-//	 * Wenn LookupEnterSelectsNextRequired gesetzt ist und das Lookup offen ist, wird der ausgewählte Wert festgesetzt und das nächste Pflichtfeld wird
-//	 * selektiert. Wenn LookupEnterSelectsNextRequired nicht gesetzt ist, wird der Wert fesetgesetzt und der Benutzer bleibt im Feld.
-//	 * <p>
-//	 * Wenn EnterSelectsFirstRequired gesetzt ist, wird das erste nicht ausgefüllte Pflichtfeld ermittelt und selektiert.
-//	 *
-//	 * @param focussedControl
-//	 *            das aktuell selektierte Feld
-//	 */
-//	private void getNextRequired(Control control, MDetail detail) {
-//		Preferences preferences = InstanceScope.INSTANCE.getNode(ApplicationPreferences.PREFERENCES_NODE);
-//		boolean lookupEnterSelectsNextRequired = (boolean) InstancePreferenceAccessor.getValue(preferences,
-//				ApplicationPreferences.LOOKUP_ENTER_SELECTS_NEXT_REQUIRED, DisplayType.CHECK, true, locale);
-//		boolean enterSelectsFirstRequired = (boolean) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.ENTER_SELECTS_FIRST_REQUIRED,
-//				DisplayType.CHECK, true, locale);
-//
-//		Control focussedControl = null;
-//
-//		if (control.getParent() instanceof TextAssist || control.getParent() instanceof Lookup) {
-//			focussedControl = control.getParent();
-//		} else {
-//			focussedControl = control;
-//		}
-//
-//		boolean popupOpen = false;
-//		if (focussedControl instanceof Lookup) {
-//			Lookup lookup = (Lookup) focussedControl;
-//			// Wir holen uns den Status des Popup des Lookup
-//			popupOpen = lookup.popupIsOpen();
-//		}
-//		if (focussedControl instanceof TextAssist) {
-//			popupOpen = false;
-//		}
-//
-//		// Wir holen uns das MField des selektierten Felds.
-//		List<MSection> sectionList = detail.getPageList();
-//		boolean isNatTable = false;
-//		if (focussedControl instanceof NatTable) {
-//			isNatTable = true;
-//		} else {
-//			selectedField = (MField) focussedControl.getData(Constants.CONTROL_FIELD);
-//		}
-//
-//		if (isNatTable) {
-//			getNextRequiredNatTableCell(focussedControl);
-//		} else {
-//
-//			// Wir prüfen ob die Preference LookupEnterSelectsNextRequired nicht gesetzt ist und das Lookup offen ist.
-//			if (!lookupEnterSelectsNextRequired && popupOpen) {
-//				focussedControl.setFocus();
-//				if (focussedControl instanceof Lookup) {
-//					Lookup lookup = (Lookup) focussedControl;
-//					lookup.closePopup();
-//					MField field = (MField) focussedControl.getData(Constants.CONTROL_FIELD);
-//					setLookupValue(field, lookup);
-//				}
-//				return;
-//			}
-//
-//			// Wir prüfen ob die Preference EnterSelectsFirstRequired gesetzt ist.
-//			if (!enterSelectsFirstRequired || popupOpen) {
-//				Control fc = null;
-//
-//				Lookup lookup = null;
-//				if (focussedControl instanceof Lookup) {
-//					lookup = (Lookup) focussedControl;
-//					if (popupOpen) {
-//						setLookupValue(selectedField, lookup);
-//					} else {
-//						selectedField.setValue(selectedField.getValue(), false);
-//					}
-//				}
-//
-//				List<MField> tabListFromSelectedFieldSection = selectedField.getmSection().getTabList();
-//				// [0,1,2,3,4,5,6,7,8,9] --> sublist(0,5) = [0,1,2,3,4]
-//				// Size = 10
-//				int indexOfSelectedField = tabListFromSelectedFieldSection.indexOf(selectedField);
-//				// Sind auf der selben Section nach meinem Feld noch unausgefüllte Required Fields?
-//				fc = getNextRequiredFieldWhichNull(tabListFromSelectedFieldSection.subList(indexOfSelectedField + 1, tabListFromSelectedFieldSection.size()));
-//				if (fc != null) {
-//					return;
-//				}
-//
-//				int indexOfSection = sectionList.indexOf(selectedField.getmSection());
-//
-//				fc = getNextControlFromSectionIfNull(selectedField, sectionList.subList(indexOfSection + 1, sectionList.size()));
-//				if (fc != null) {
-//					return;
-//				}
-//				fc = getNextControlFromSectionIfNull(selectedField, sectionList.subList(0, indexOfSection));
-//				if (fc != null) {
-//					return;
-//				}
-//				// Sind auf der selben Section vor meinem Feld noch unausgefüllte Required Fields?
-//				fc = getNextRequiredFieldWhichNull(tabListFromSelectedFieldSection.subList(0, indexOfSelectedField));
-//				if (fc == null) {
-//					if (focussedControl instanceof Lookup) {
-//						lookup.closePopup();
-//					}
-//					return;
-//				}
-//
-//			} else {
-//				Lookup lookup = null;
-//				if (focussedControl instanceof Lookup) {
-//					lookup = (Lookup) focussedControl;
-//					if (popupOpen) {
-//						setLookupValue(selectedField, lookup);
-//					} else {
-//						selectedField.setValue(selectedField.getValue(), false);
-//					}
-//				}
-//
-//				for (MSection mSection : sectionList) {
-//					List<MField> tabList = mSection.getTabList();
-//					for (MField field : tabList) {
-//						if (field.isRequired() && field.getValue() == null && !field.isReadOnly()) {
-//							if (field instanceof MLookupField && ((MLookupField) field).getWrittenText() != null) {
-//								continue;
-//							}
-//							Section section = field.getmSection().getSection();
-//							// Prüfen, ob die Section in der das nächste Pflichtfeld sich befindet geschlossen ist
-//							if (!section.isExpanded()) {
-//								// Section öffnen
-//								section.setExpanded(true);
-//							}
-//							focussedControl = ((AbstractValueAccessor) field.getValueAccessor()).getControl();
-//							focussedControl.setFocus();
-//							return;
-//						}
-//					}
-//				}
-//			}
-//		}
-//		return;
-//	}
-//
-
-//
-//	private Control getNextControlFromSectionIfNull(MField selectedField, List<MSection> sectionList) {
-//		Control focussedControl = null;
-//		for (MSection section : sectionList) {
-//			List<MField> tabList = section.getTabList();
-//
-//			if (selectedField.getmSection().equals(section)) {
-//				int indexOfSelectedField = tabList.indexOf(selectedField);
-//
-//				focussedControl = getNextRequiredFieldWhichNull(tabList.subList(indexOfSelectedField, tabList.size() - 1));
-//				if (focussedControl != null) {
-//					return focussedControl;
-//				}
-//				focussedControl = getNextRequiredFieldWhichNull(tabList.subList(0, indexOfSelectedField));
-//				if (focussedControl != null) {
-//					return focussedControl;
-//				}
-//			} else {
-//				focussedControl = getNextRequiredFieldWhichNull(tabList);
-//				if (focussedControl != null) {
-//					return focussedControl;
-//				}
-//			}
-//		}
-//		return focussedControl;
-//	}
-//
-//	private Control getNextRequiredFieldWhichNull(List<MField> tabListAfter) {
-//		Control focussedControl = null;
-//		for (MField field : tabListAfter) {
-//			// 1. mein Feld kommt nach dem aktuellen INIT_FIELD ##
-//			// 2. Mein Feld kommt nach dem aktuellen INIT_FIELD, auf nächster Section ##
-//			// 3. Mein Feld kommt vor dem aktuellen INIT_FIELD, auf vorheriger Section ##
-//			// 4. Mein Feld kommt vor dem aktuellen INIT_FIELD, auf gleicher Section ##
-//			if (field.getValue() == null && field.isRequired() && !field.isReadOnly()) {
-//				focussedControl = ((AbstractValueAccessor) field.getValueAccessor()).getControl();
-//				Section section = field.getmSection().getSection();
-//				// Prüfen, ob die Section in der das nächste Pflichtfeld sich befindet geschlossen ist
-//				if (!section.isExpanded()) {
-//					// Section öffnen
-//					section.setExpanded(true);
-//				}
-//				focussedControl.setFocus();
-//				return focussedControl;
-//			}
-//		}
-//		return focussedControl;
-//	}
-//
 	private void setLookupValue(MField field, Lookup lookup) {
 		LookupValue lv = null;
 		if (lookup.getTable().getSelectionIndex() > 0) {
