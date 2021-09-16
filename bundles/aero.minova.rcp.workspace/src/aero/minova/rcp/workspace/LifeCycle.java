f8404cf093534dad35b5d3fee6cc22e9432f8d6f
Lea Janiak <janiak@minova.aero>
2021-09-16 14:03:49 +0200

    Wenn Einloggen mit DefaultWorkspace nicht möglich ist LoginDialog öffnen

--- bundles/aero.minova.rcp.workspace/src/aero/minova/rcp/workspace/LifeCycle.java
+++ bundles/aero.minova.rcp.workspace/src/aero/minova/rcp/workspace/LifeCycle.java
@@ -19,6 +19,7 @@ import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.equinox.security.storage.ISecurePreferences;
+import org.eclipse.equinox.security.storage.StorageException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.osgi.framework.Bundle;
@@ -29,6 +30,7 @@ import aero.minova.rcp.dataservice.IDataService;
 import aero.minova.rcp.preferences.WorkspaceAccessPreferences;
 import aero.minova.rcp.translate.lifecycle.Manager;
 import aero.minova.rcp.workspace.dialogs.WorkspaceDialog;
+import aero.minova.rcp.workspace.handler.WorkspaceHandler;
 
 @SuppressWarnings("restriction")
 public class LifeCycle {
@@ -43,21 +45,124 @@ public class LifeCycle {
 	IDataService dataService;
 
 	@PostContextCreate
-	void postContextCreate(IEclipseContext workbenchContext) throws IllegalStateException, IOException {
+	void postContextCreate(IEclipseContext workbenchContext) throws IllegalStateException {
 		URI workspaceLocation = null;
 
-		// Auslesen der übergabenen ProgrammArgumente
-		String[] applicationArgs = Platform.getApplicationArgs();
+		// Bei -clearPersistedState müssen unsere Einstellungen auch gelöscht werden
+		boolean deletePrefs = false;
+		for (String string : Platform.getApplicationArgs()) {
+			if (string.equals("-clearPersistedState")) {
+				deletePrefs = true;
+			}
+		}
+
+		// Versuchen über Commandline-Argumente einzuloggen, für UI-Tests genutzt
+		boolean loginCommandLine = loginViaCommandLine(workbenchContext);
+
+		if (!loginCommandLine) {
+
+			WorkspaceDialog workspaceDialog = new WorkspaceDialog(null, logger, sync);
+
+			if (!WorkspaceAccessPreferences.getSavedPrimaryWorkspaceAccessData(logger).isEmpty()) {
+				// Wenn Default-Workspace gesetzt ist diesen nutzen
+				workspaceLocation = loginDefaultWorkspace(workspaceLocation, workspaceDialog);
+			} else {
+				// Ansonsten sofort Login-Dialog öffnen
+				workspaceLocation = loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
+			}
+
+			checkModelVersion(workspaceLocation);
+		}
+
+		if (deletePrefs) {
+			deleteCustomPrefs(workspaceLocation);
+		}
+
+		Manager manager = new Manager();
+		manager.postContextCreate(workbenchContext);
+	}
+
+	/**
+	 * Versuchen über Default-Daten einzuloggen. Bei Fehlschlag wird Login-Dialog geöffnet
+	 * 
+	 * @param workspaceLocation
+	 * @param workspaceDialog
+	 * @return
+	 */
+	private URI loginDefaultWorkspace(URI workspaceLocation, WorkspaceDialog workspaceDialog) {
+		try {
+			ISecurePreferences sPrefs = WorkspaceAccessPreferences.getSavedPrimaryWorkspaceAccessData(logger).get();
+			if (!Platform.getInstanceLocation().isSet()) {
+				Platform.getInstanceLocation().set(new URL(sPrefs.get(WorkspaceAccessPreferences.APPLICATION_AREA, null)), false);
+				try {
+					workspaceLocation = Platform.getInstanceLocation().getURL().toURI();
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+
+				if (workspaceLocation == null) {
+					WorkspaceAccessPreferences.resetDefaultWorkspace(logger);
+					loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
+				} else {
+
+					workspaceLocation = checkDefaultWorkspace(workspaceLocation, workspaceDialog, sPrefs);
+
+				}
+			}
+		} catch (Exception e) {
+			logger.error(e);
+			workspaceLocation = loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
+		}
+		return workspaceLocation;
+	}
+
+	/**
+	 * Überprüfen, ob die Default-Login-Daten gültig sind. Ansonsten Öffnen des Default-Workspaces
+	 * 
+	 * @param workspaceLocation
+	 * @param workspaceDialog
+	 * @param sPrefs
+	 * @return
+	 * @throws StorageException
+	 */
+	private URI checkDefaultWorkspace(URI workspaceLocation, WorkspaceDialog workspaceDialog, ISecurePreferences sPrefs) throws StorageException {
+		String username = sPrefs.get(WorkspaceAccessPreferences.USER, null);
+		String pw = sPrefs.get(WorkspaceAccessPreferences.PASSWORD, null);
+		String url = sPrefs.get(WorkspaceAccessPreferences.URL, null);
+
+		try {
+			WorkspaceHandler workspaceHandler = WorkspaceHandler.newInstance(sPrefs.name(), url, logger);
+			workspaceHandler.checkConnection(username, pw, workspaceLocation.toString(), true);
+			workspaceHandler.open();
+
+			dataService.setCredentials(username, pw, url, workspaceLocation);
+			dataService.setLogger(logger);
+		} catch (WorkspaceException e) {
+			workspaceDialog = new WorkspaceDialog(null, logger, sync, sPrefs.name());
+			workspaceLocation = loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
+		}
+
+		return workspaceLocation;
+
+	}
+
+	/**
+	 * Versucht User, PW und URL aus commandline auszulesen. Wenn erfolgreich, werden diese genutzt und es öffnet sich kein Login-Dialog
+	 * 
+	 * @param workbenchContext
+	 * @return
+	 */
+	private boolean loginViaCommandLine(IEclipseContext workbenchContext) {
 
 		String argUser = null;// "admin";
 		String argPW = null;// "rqgzxTf71EAx8chvchMi";
 		String argURL = null;// "http://publictest.minova.com:17280/cas";
 
-		boolean deletePrefs = false;
-		for (String string : applicationArgs) {
+		// Auslesen der übergabenen ProgrammArgumente
+		for (String string : Platform.getApplicationArgs()) {
 			if (string.startsWith("-user=")) {
 				argUser = string.substring(string.indexOf("=") + 1);
-				// In UI-Tests darf sich der Dialog nicht öffnen
+				// In UI-Tests darf sich der "UI wird wiederhergestellt" Dialog nicht öffnen
 				workbenchContext.set(Constants.NEVER_SHOW_RESTORING_UI_MESSAGE, true);
 			}
 			if (string.startsWith("-pw=")) {
@@ -66,56 +171,20 @@ public class LifeCycle {
 			if (string.startsWith("-url=")) {
 				argURL = string.substring(string.indexOf("=") + 1);
 			}
-			if (string.equals("-clearPersistedState")) {
-				deletePrefs = true;
-			}
 		}
 
 		if (argPW != null && argURL != null && argUser != null) {
 			try {
-				workspaceLocation = Platform.getInstanceLocation().getURL().toURI();
+				URI workspaceLocation = Platform.getInstanceLocation().getURL().toURI();
+				dataService.setCredentials(argUser, argPW, argURL, workspaceLocation);
+				dataService.setLogger(logger);
+				return true;
 			} catch (URISyntaxException e) {
 				e.printStackTrace();
 			}
-			dataService.setCredentials(argUser, argPW, argURL, workspaceLocation);
-			dataService.setLogger(logger);
-		} else {
-			WorkspaceDialog workspaceDialog = new WorkspaceDialog(null, logger, sync);
-
-			if (!WorkspaceAccessPreferences.getSavedPrimaryWorkspaceAccessData(logger).isEmpty()) {
-				try {
-					ISecurePreferences sPrefs = WorkspaceAccessPreferences.getSavedPrimaryWorkspaceAccessData(logger).get();
-					if (!Platform.getInstanceLocation().isSet()) {
-						Platform.getInstanceLocation().set(new URL(sPrefs.get(WorkspaceAccessPreferences.APPLICATION_AREA, null)), false);
-						try {
-							workspaceLocation = Platform.getInstanceLocation().getURL().toURI();
-						} catch (URISyntaxException e) {
-							e.printStackTrace();
-						}
-						if (workspaceLocation == null) {
-							WorkspaceAccessPreferences.resetDefaultWorkspace(logger);
-							loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
-						} else {
-							dataService.setCredentials(sPrefs.get(WorkspaceAccessPreferences.USER, null), sPrefs.get(WorkspaceAccessPreferences.PASSWORD, null),
-									sPrefs.get(WorkspaceAccessPreferences.URL, null), workspaceLocation);
-							dataService.setLogger(logger);
-						}
-					}
-				} catch (Exception e) {
-					logger.error(e);
-					workspaceLocation = loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
-				}
-			} else {
-				workspaceLocation = loadWorkspaceConfigManually(workspaceDialog, workspaceLocation);
-			}
-			checkModelVersion(workspaceLocation);
-			if (deletePrefs) {
-				deleteCustomPrefs(workspaceLocation);
-			}
 		}
 
-		Manager manager = new Manager();
-		manager.postContextCreate(workbenchContext);
+		return false;
 	}
 
 	/**
