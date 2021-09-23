a613d5f6cd1e558582f084d688adf7c8f8ce194a
Bauer-David <bauer@minova.com>
2021-09-23 14:25:52 +0200

    getDate/Time/DateTimestring um Formatter erweitern

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/ShortDateDisplayConverter.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/ShortDateDisplayConverter.java
@@ -3,8 +3,13 @@ package aero.minova.rcp.rcp.nattable;
 import java.time.Instant;
 import java.util.Locale;
 
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.nebula.widgets.nattable.data.convert.DisplayConverter;
+import org.osgi.service.prefs.Preferences;
 
+import aero.minova.rcp.preferences.ApplicationPreferences;
+import aero.minova.rcp.preferencewindow.builder.DisplayType;
+import aero.minova.rcp.preferencewindow.builder.InstancePreferenceAccessor;
 import aero.minova.rcp.util.DateUtil;
 
 public class ShortDateDisplayConverter extends DisplayConverter {
@@ -17,8 +22,11 @@ public class ShortDateDisplayConverter extends DisplayConverter {
 
 	@Override
 	public Object canonicalToDisplayValue(Object canonicalValue) {
+		Preferences preferences = InstanceScope.INSTANCE.getNode(ApplicationPreferences.PREFERENCES_NODE);
+		String dateUtil = (String) InstancePreferenceAccessor.getValue(preferences, ApplicationPreferences.DATE_UTIL, DisplayType.DATE_UTIL, "", locale);
+
 		if (canonicalValue instanceof Instant) {
-			return DateUtil.getDateString((Instant) canonicalValue, locale);
+			return DateUtil.getDateString((Instant) canonicalValue, locale, dateUtil);
 		}
 		return null;
 	}
