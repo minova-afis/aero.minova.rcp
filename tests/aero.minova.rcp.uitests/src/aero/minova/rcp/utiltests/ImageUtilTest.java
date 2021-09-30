745f2aa90f1e7eaed6369cc86e9c77ce95370985
Lea Janiak <janiak@minova.aero>
2021-09-30 16:53:03 +0200

    Init auskommentieren

--- tests/aero.minova.rcp.uitests/src/aero/minova/rcp/utiltests/ImageUtilTest.java
+++ tests/aero.minova.rcp.uitests/src/aero/minova/rcp/utiltests/ImageUtilTest.java
@@ -2,21 +2,18 @@ package aero.minova.rcp.utiltests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import aero.minova.rcp.dataservice.ImageUtil;
-import aero.minova.rcp.preferences.ApplicationPreferences;
 
 class ImageUtilTest {
 
 	@BeforeEach
-	@Ignore
 	void init() {
-		InstanceScope.INSTANCE.getNode(ApplicationPreferences.PREFERENCES_NODE).put(ApplicationPreferences.FONT_SIZE, "S");
+		// InstanceScope.INSTANCE.getNode(ApplicationPreferences.PREFERENCES_NODE).put(ApplicationPreferences.FONT_SIZE, "S");
 	}
 
 	@Test
