148374081e61067cf9ada0406f830cb71ba604fc
Christopher Erlanger <erlanger@minova.com>
2021-09-30 08:03:44 +0200

    Laden der images über den standardmechanismus

---
+++ bundles/aero.minova.rcp.dataservice/src/aero/minova/rcp/dataservice/internal/MinovaImageService.java
@@ -0,0 +1,32 @@
+package aero.minova.rcp.dataservice.internal;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import aero.minova.rcp.dataservice.IDataService;
+import aero.minova.rcp.dataservice.IDummyService;
+
+@Component(immediate = true)
+public class MinovaImageService {
+
+	private IDataService dataService;
+
+	@Reference
+	void getDummyService(IDummyService dummyService) {
+		// this method prevents the immediate component to be actived before the
+		// workspace location in the dataservice has been set
+		// maybe move that to a service property dependency?
+	}
+
+	@Reference
+	void getDataService(IDataService dataService) {
+		this.dataService = dataService;
+	}
+
+	@Activate
+	public void activatePlugin() {
+		dataService.getHashedZip("Images.zip");
+	}
+
+}
