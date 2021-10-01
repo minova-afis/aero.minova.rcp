1e182d3adf42f247dc65352ffad18bf515beebe0
Bauer-David <bauer@minova.com>
2021-10-01 09:25:19 +0200

    DateUtilTest korrigieren

--- tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/util/DateUtilTests.java
+++ tests/aero.minova.rcp.rcp.tests/src/aero/minova/rcp/rcp/util/DateUtilTests.java
@@ -201,7 +201,7 @@ public class DateUtilTests {
 
 	@Test
 	public void testGetDate07_01_20() {
-		Instant bithday = LocalDate.of(1920, MAY, 13).atStartOfDay().plusHours(18).plusMinutes(12).toInstant(ZoneOffset.UTC);
+		Instant bithday = LocalDate.of(2020, MAY, 13).atStartOfDay().plusHours(18).plusMinutes(12).toInstant(ZoneOffset.UTC);
 		Instant expected = LocalDate.of(2020, JANUARY, 7).atStartOfDay().toInstant(ZoneOffset.UTC);
 		assertEquals(expected, DateUtil.getDate(bithday, "07.01.20", Locale.GERMANY, ""));
 	}
@@ -311,11 +311,5 @@ public class DateUtilTests {
 	@Test(expected = IllegalArgumentException.class)
 	public void testShortcut2WeekCahrs() {
 		DateUtil.setShortcuts("t", "m", "y", "ww");
-	}
-
-	@Test
-	public void testGetDate02Point05() {
-		Instant bithday = LocalDate.of(1967, MAY, 23).atStartOfDay().plusHours(18).plusMinutes(12).toInstant(ZoneOffset.UTC);
-		assertNull(DateUtil.getDate(bithday, "02.05"));
 	}
 }
