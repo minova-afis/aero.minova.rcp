78c667157c539fc7dada4f4169f0020ba726136a
Lea Janiak <janiak@minova.aero>
2021-10-20 12:53:57 +0200

    Rowindex in Tabelle finden

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/gridvalidation/CrossValidationLabelAccumulator.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/gridvalidation/CrossValidationLabelAccumulator.java
@@ -8,6 +8,8 @@ import org.eclipse.nebula.widgets.nattable.layer.cell.ColumnLabelAccumulator;
 import org.eclipse.nebula.widgets.nattable.layer.cell.IConfigLabelAccumulator;
 
 import aero.minova.rcp.constants.Constants;
+import aero.minova.rcp.model.Row;
+import aero.minova.rcp.model.Table;
 import aero.minova.rcp.model.form.IGridValidator;
 
 /**
@@ -19,26 +21,28 @@ public class CrossValidationLabelAccumulator extends ColumnLabelAccumulator {
 	private IGridValidator validator;
 
 	ILayer layer;
+	private List<Row> sortedList;
+	private Table dataTable;
 
-	public CrossValidationLabelAccumulator(ILayer layer, IGridValidator validator, List<Integer> columnsToValidate) {
+	public CrossValidationLabelAccumulator(ILayer layer, IGridValidator validator, List<Integer> columnsToValidate, List<Row> sortedList, Table dataTable) {
 		this.validator = validator;
 		this.columnsToValidate = columnsToValidate;
 		this.layer = layer;
+		this.sortedList = sortedList;
+		this.dataTable = dataTable;
 	}
 
 	@Override
 	public void accumulateConfigLabels(LabelStack configLabels, int columnPosition, int rowPosition) {
-		int columnIndex = layer.getColumnIndexByPosition(columnPosition);
-		int rowIndex = layer.getRowIndexByPosition(rowPosition);
-		super.accumulateConfigLabels(configLabels, columnIndex, rowIndex);
+		super.accumulateConfigLabels(configLabels, columnPosition, rowPosition);
+
+		// Da die Tabelle nicht sortiert wird müssen wir den entsprechenden Index herausfinden
+		int rowIndexInDataTable = dataTable.getRows().indexOf(sortedList.get(rowPosition));
 
 		// Angegebenen Spalten überprüfen
-		if (columnsToValidate.contains(columnIndex)) {
+		if (columnsToValidate.contains(columnPosition)) {
 			configLabels.addLabel(Constants.VALIDATION_CELL_LABEL);
-
-			if (!validator.checkValid(columnIndex, rowIndex)) {
-				System.out.println(columnIndex + " " + rowIndex + " Label " + columnPosition + " " + rowPosition + " "
-						+ layer.getDataValueByPosition(columnPosition, rowPosition));
+			if (!validator.checkValid(columnPosition, rowIndexInDataTable)) {
 				configLabels.addLabel(Constants.INVALID_CELL_LABEL);
 			}
 		}
