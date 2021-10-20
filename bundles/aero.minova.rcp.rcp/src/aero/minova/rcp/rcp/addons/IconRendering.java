3247831c083faa9de55f647f7d8c7b3339f063b4
Lars Vogel <Lars.Vogel@vogella.com>
2021-10-20 12:16:21 +0200

    Update the toolbar icons
    Update the toolbar icons
    
    Bein Erzeugen der Toolbareinträge werden die Icons gesetzt und nicht
    mehr wie bisher beim Wechsel des Parts. Das führt zu einer besseren
    interactive Performance und vermeidet, dass wir die Toolbars
    für Bug
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=576751 entfernen und
    hinzufügen müssen, um ein Update zu erzwingen. Dieses Entfernen und
    Hinzufügen hat zu NPE bei den UI Test und unter Mac geführt.

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/addons/IconRendering.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/addons/IconRendering.java
@@ -5,7 +5,13 @@ import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.EventTopic;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
+import org.eclipse.e4.ui.model.application.ui.menu.MPopupMenu;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.osgi.service.event.Event;
@@ -14,9 +20,39 @@ import aero.minova.rcp.dataservice.ImageUtil;
 
 public class IconRendering {
 
+	@Inject 
+	@Optional 
+	EModelService modelService;
+	
 	@Inject
 	@Optional
-	public void applicationStarted(@EventTopic(UIEvents.UILifeCycle.ACTIVATE) Event event, MApplication mApp, EModelService eModelService) {
-		ImageUtil.updateModelIcons(mApp, eModelService);
+	void subscribeTopicChildrenChanged(@UIEventTopic(UIEvents.ElementContainer.TOPIC_CHILDREN) Event event) {
+		Object objElement = event.getProperty(UIEvents.EventTags.ELEMENT);
+
+		// only care of added elements
+				if (!UIEvents.isADD(event)) {
+					return;
+				} 
+		// Ensure that this event is for a MMenuItem or MToolBar
+		if (!(objElement instanceof MMenuElement && !(objElement instanceof MPopupMenu))
+				&& !(objElement instanceof MToolBar)) {
+			return;
+		}
+		
+		
+		// Ensure that it's a View part's menu or toolbar
+		MUIElement uiElement = (MUIElement) objElement;
+		MUIElement parent = modelService.getContainer(uiElement);
+		if (!(parent instanceof MPart)) {
+			return;
+		} 
+		
+		MPart part = (MPart) parent;
+		ImageUtil.updateIconsForPart(part);
+		if (uiElement instanceof MToolBar) {
+			MToolBar toolbar = (MToolBar) uiElement;
+			ImageUtil.updateIconsForToolbarItems(toolbar);
+		}
+
 	}
 }
