0bea9ef4f1e34cf2a5fb152841f4b78c39c2602c
Christopher Erlanger <erlanger@minova.com>
2021-11-10 16:32:12 +0100

    entfernt den localResourceManager der zu Fehlern führte

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/PerspectiveControl.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/PerspectiveControl.java
@@ -28,8 +28,6 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MenuEvent;
 import org.eclipse.swt.events.MenuListener;
@@ -86,14 +84,11 @@ public class PerspectiveControl {
 	Preferences prefsToolbarOrder = InstanceScope.INSTANCE.getNode(Constants.PREFERENCES_TOOLBARORDER);
 	List<String> openToolbarItems;
 
-	private LocalResourceManager localResourceManager;
-
 	/*
 	 * Create the ToolControl with a Toolbar for the Perspective Shortcuts
 	 */
 	@PostConstruct
 	public void createGui(Composite parent, MWindow window) {
-		localResourceManager = new LocalResourceManager(JFaceResources.getResources(), parent);
 
 		composite = new Composite(parent, SWT.BAR);
 		RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
@@ -190,9 +185,9 @@ public class PerspectiveControl {
 			shortcut = new ToolItem(toolBar, SWT.RADIO);
 			shortcut.setData(perspectiveId);
 			ImageDescriptor descriptor = ImageUtil.getImageDescriptor(iconURI, true);
-			
+
 			if (descriptor != null && !descriptor.equals(ImageDescriptor.getMissingImageDescriptor())) {
-				shortcut.setImage(localResourceManager.createImage(descriptor));
+				shortcut.setImage(descriptor.createImage());
 			} else {
 				shortcut.setText(localizedLabel != null ? localizedLabel : "");
 			}
