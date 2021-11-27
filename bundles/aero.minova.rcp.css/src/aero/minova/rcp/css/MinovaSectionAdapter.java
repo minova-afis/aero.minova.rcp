0c85b8adb8e0e44f538715f34a108b2534b529af
Wilfried Saak <saak@minova.aero>
2021-11-27 18:39:36 +0100

    Styling aktivieren

---
+++ bundles/aero.minova.rcp.css/src/aero/minova/rcp/css/MinovaSectionAdapter.java
@@ -0,0 +1,15 @@
+package aero.minova.rcp.css;
+
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.eclipse.e4.ui.css.swt.dom.CompositeElement;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author Wilfried Saak
+ */
+@SuppressWarnings("restriction")
+public class MinovaSectionAdapter extends CompositeElement {
+	public MinovaSectionAdapter(Composite composite, CSSEngine engine) {
+		super(composite, engine);
+	}
+}
