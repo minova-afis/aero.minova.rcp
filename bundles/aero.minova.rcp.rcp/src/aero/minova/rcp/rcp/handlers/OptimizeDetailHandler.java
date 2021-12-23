e5d995689b561f5c6b1446e76e1e84237e0480e3
Lea Janiak <janiak@minova.aero>
2021-12-23 11:55:17 +0100

    "Optimieren" soll Fenstergröße unverändert lassen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeDetailHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/OptimizeDetailHandler.java
@@ -5,7 +5,6 @@ import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainerElement;
-import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.widgets.Composite;
@@ -20,23 +19,6 @@ public class OptimizeDetailHandler {
 	@Execute
 	public void execute(EModelService emservice, MWindow mwindow, MPerspective mPerspective) {
 		MPart detail = emservice.findElements(emservice.getActivePerspective(mwindow), "aero.minova.rcp.rcp.part.details", MPart.class).get(0);
-
-		// Höhe mindestens 900 Pixel
-		if (mwindow.getHeight() < 900) {
-			mwindow.setHeight(900);
-
-			try {
-				// Höhe von Suche und Index im Verhältnis 35/65
-				MPartStack searchPartStack = emservice
-						.findElements(emservice.getActivePerspective(mwindow), "aero.minova.rcp.rcp.partstack.search", MPartStack.class).get(0);
-				searchPartStack.setContainerData("35");
-				MPartStack indexPartStack = emservice
-						.findElements(emservice.getActivePerspective(mwindow), "aero.minova.rcp.rcp.partstack.index", MPartStack.class).get(0);
-				indexPartStack.setContainerData("65");
-			} catch (IndexOutOfBoundsException e) {
-				// In "Statistic" Ansicht haben wir links nur ein Part -> nichts tun
-			}
-		}
 
 		Integer defaultSectionWidth = (Integer) mPerspective.getContext().get(Constants.DETAIL_WIDTH);
 
@@ -53,11 +35,6 @@ public class OptimizeDetailHandler {
 		// Optimale Breite des Details. Dabei werden die horizontalFill-Abschnitte ignoriert
 		DetailLayout detailLayout = (DetailLayout) mPerspective.getContext().get(Constants.DETAIL_LAYOUT);
 		int prefDetailWidth = detailLayout.layout(detailComposite, false, detailComposite.getSize().x, false, false).x;
-
-		// Standardbreite defaultSectionWidth (für Index/Suche) + Benötigte Breite für Detail (können mehrere Sections nebeneinander sein) + 50 Pixel
-		if (mwindow.getWidth() < defaultSectionWidth + prefDetailWidth + 50) {
-			mwindow.setWidth(defaultSectionWidth + prefDetailWidth + 50);
-		}
 
 		int size = 100000;
 		float detailSize = (float) (prefDetailWidth + 35.0) / mwindow.getWidth();
