58d9b7aeccc53587550623f3a0e270f4b3c899bc
Lea Janiak <janiak@minova.aero>
2022-01-27 15:42:04 +0100

    Tabelle um TableMetaData erweitern (für Anfragen ans CAS)

--- bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/Table.java
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/Table.java
@@ -8,11 +8,7 @@ import java.util.List;
 public class Table {
 
 	String name;
-	// vDriverIndex
-	// opReadDriverRights
-	// spReadDriver
-	// opReadContact
-	//
+	private TableMetaData metaData;
 	List<Column> columns = new ArrayList<>();
 	List<Row> rows = new ArrayList<>();
 
@@ -242,5 +238,37 @@ public class Table {
 				}
 			}
 		}
+	}
+
+	public void fillMetaData(Integer limit, Integer totalResults, Integer page) {
+		TableMetaData metaData = getMetaData();
+		if (metaData == null) {
+			metaData = new TableMetaData();
+		}
+		if (limit <= 0) {
+			limit = totalResults;
+		}
+		if (totalResults != null && limit != null && totalResults > 0 && limit > 0) {
+			int totalPages = (int) Math.ceil(totalResults / (double) limit);
+			metaData.setResultsLeft(Math.max(totalResults - (page * limit), 0));
+			metaData.setTotalPages(totalPages);
+		}
+		metaData.setLimited(limit);
+		metaData.setPage(page);
+		metaData.setTotalResults(totalResults);
+
+		this.setMetaData(metaData);
+	}
+
+	public TableMetaData getMetaData() {
+		return metaData;
+	}
+
+	public void setMetaData(TableMetaData metaData) {
+		this.metaData = metaData;
+	}
+
+	public void setMetaDataLimit(int limit) {
+		metaData.setLimited(limit);
 	}
 }
