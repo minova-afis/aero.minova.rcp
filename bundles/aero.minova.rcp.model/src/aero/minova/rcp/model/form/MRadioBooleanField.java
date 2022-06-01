0d6611186cb206469ead84a7d733f466919c5482
Christopher Erlanger <erlanger@minova.com>
2022-06-01 17:59:02 +0200

    Seteun des String-Wertes für den Fall dass wir eine RadioBox angehakt..

--- bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/form/MRadioBooleanField.java
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/form/MRadioBooleanField.java
@@ -3,6 +3,7 @@ package aero.minova.rcp.model.form;
 import java.util.ArrayList;
 
 import aero.minova.rcp.model.DataType;
+import aero.minova.rcp.model.Value;
 
 public class MRadioBooleanField extends MField{
 
@@ -18,6 +19,19 @@ public class MRadioBooleanField extends MField{
 
 	public void setRadiobuttons(ArrayList<MBooleanField> radiobuttons) {
 		this.radiobuttons = radiobuttons;
+	}
+
+	@Override
+	public void setValue(Value value, boolean user) {
+		// Boolean Felder dürfen keinen null-Value haben, weil dies nicht dargestellt werden kann
+		value = new Value(new String());
+		for (MBooleanField b : radiobuttons) {
+			if (b.getValue().getBooleanValue()) {
+				super.setValue(new Value(b.getName()), user);
+				return;
+			}
+		}
+		super.setValue(value, user);
 	}
 
 }
