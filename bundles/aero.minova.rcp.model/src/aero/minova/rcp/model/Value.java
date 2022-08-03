67769b04da752aa2b1e1c7deaaa0692a7b399d1d
Lea Janiak <janiak@minova.aero>
2022-08-03 12:11:57 +0200

    Equals ausbessern

--- bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/Value.java
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/Value.java
@@ -220,6 +220,12 @@ public class Value implements Serializable {
 
 	@Override
 	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
 		if (getClass() != obj.getClass()) {
 			return false;
 		}
