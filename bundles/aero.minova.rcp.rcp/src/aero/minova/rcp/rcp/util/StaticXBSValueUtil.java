124527c75098aab1cd44698c47133382908718b0
Lea Janiak <janiak@minova.aero>
2022-08-25 14:31:27 +0200

    XBS Einträge übersetzen, Date/Time Shortcuts parsen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/util/StaticXBSValueUtil.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/util/StaticXBSValueUtil.java
@@ -2,18 +2,41 @@ package aero.minova.rcp.rcp.util;
 
 import java.time.Instant;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+
+import org.eclipse.e4.core.services.translation.TranslationService;
 
 import aero.minova.rcp.model.DataType;
+import aero.minova.rcp.model.DateTimeType;
 import aero.minova.rcp.model.Value;
+import aero.minova.rcp.util.DateTimeUtil;
+import aero.minova.rcp.util.DateUtil;
+import aero.minova.rcp.util.TimeUtil;
 
-public class StringToValueUtil {
+/**
+ * Mit dieser Klassen können die statischen/festen Strings aus der XBS in Values umgewandelt werden (siehe #1357)
+ * 
+ * @author janiak
+ */
+public class StaticXBSValueUtil {
 
-	private StringToValueUtil() {}
+	private StaticXBSValueUtil() {}
 
-	public static Value stringToValue(String valueString, DataType dataType) {
+	/**
+	 * Erstellt einen Value mit gegebenen Datentyp aus einem String. Sonderfälle:
+	 * <li>String-Datentyp beginnend mit "@": wird übersetzt</li>
+	 * <li>Für Datums-/Zeitvalues können bekannte Abkürzungen verwendet werden ("0", "1212", ...)</li>
+	 * 
+	 * @param valueString
+	 * @param dataType
+	 * @return
+	 */
+	public static Value stringToValue(String valueString, DataType dataType, DateTimeType dateTimeType, TranslationService translationService,
+			String timezone) {
+
 		switch (dataType) {
 		case STRING:
-			return new Value(valueString);
+			return new Value(translationService.translate(valueString, null));
 		case BIGDECIMAL:
 			return new Value(Double.valueOf(valueString), DataType.BIGDECIMAL);
 		case BOOLEAN:
@@ -24,7 +47,22 @@ public class StringToValueUtil {
 			// Sollte nicht vorkommen
 			break;
 		case INSTANT:
-			return new Value(Instant.parse(valueString), DataType.INSTANT);
+
+			try {
+				return new Value(Instant.parse(valueString), DataType.INSTANT);
+			} catch (DateTimeParseException e) {
+				// Versuchen, Abkürzung zu parsen
+			}
+
+			switch (dateTimeType) {
+			case DATE:
+				return new Value(DateUtil.getDate(valueString), DataType.INSTANT);
+			case DATETIME:
+				return new Value(DateTimeUtil.getDateTime(valueString, timezone), DataType.INSTANT);
+			case TIME:
+				return new Value(TimeUtil.getTime(valueString), DataType.INSTANT);
+			}
+			return null;
 		case INTEGER:
 			return new Value(Integer.valueOf(valueString), DataType.INTEGER);
 		case PERIOD:
