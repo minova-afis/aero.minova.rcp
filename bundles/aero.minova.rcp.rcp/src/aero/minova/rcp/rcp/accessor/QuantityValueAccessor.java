23baa8cb90349b961f54f68eaec14bd9d1524cd0
David Bauer <bauer@minova.de>
2022-11-09 10:42:41 +0100

    NumberFormatUtil verwenden

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/accessor/QuantityValueAccessor.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/accessor/QuantityValueAccessor.java
@@ -1,5 +1,6 @@
 package aero.minova.rcp.rcp.accessor;
 
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -14,19 +15,16 @@ import aero.minova.rcp.model.DataType;
 import aero.minova.rcp.model.Value;
 import aero.minova.rcp.model.form.MQuantityField;
 import aero.minova.rcp.rcp.fields.FieldUtil;
+import aero.minova.rcp.rcp.util.NumberFormatUtil;
+import aero.minova.rcp.rcp.util.NumberFormatUtil.Result;
 
 public class QuantityValueAccessor extends AbstractValueAccessor implements VerifyListener {
 
-	public static class Result {
-		String text;
-		int caretPosition;
-		Value value;
-	}
+	private boolean verificationActive = false;
 
 	public QuantityValueAccessor(MQuantityField field, Control control) {
 		super(field, control);
 	}
-
 
 	@Override
 	protected void updateControlFromValue(Control control, Value value) {
@@ -43,10 +41,10 @@ public class QuantityValueAccessor extends AbstractValueAccessor implements Veri
 			NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
 			numberFormat.setMaximumFractionDigits(decimals); // wir wollen genau so viele Nachkommastellen
 			numberFormat.setMinimumFractionDigits(decimals); // dito
-			
+
 			Pattern p = Pattern.compile("\\p{Alpha}");
 			Matcher m = p.matcher(value.getStringValue());
-			
+
 			value = new Value(value.getStringValue().substring(value.getStringValue().indexOf(m.start())), field.getDataType());
 
 			if (value.getType().equals(DataType.DOUBLE)) {
@@ -58,10 +56,65 @@ public class QuantityValueAccessor extends AbstractValueAccessor implements Veri
 			}
 		}
 	}
-
+	
 	@Override
 	public void verifyText(VerifyEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (!isFocussed()) {
+			return; // Wir sind aktiv, wenn der Control den Focus hat
+		}
+		if (verificationActive) {
+			return; // diese Methode setzt einen neuen Wert
+		}
+		if (!e.doit) {
+			return; // anscheinend hat schon jemand reagiert
+		}
+
+		// Werte vom Event
+		String insertion = e.text;
+		int start = e.start;
+		int end = e.end;
+		int keyCode = e.keyCode;
+
+		// Werte aus dem Model
+		int decimals = field.getDecimals();
+
+		// Werte vom Text-Widget
+		Text control = (Text) e.getSource();
+		Locale locale = (Locale) control.getData(FieldUtil.TRANSLATE_LOCALE);
+		boolean rangeSelected = false;
+
+		int caretPosition = control.getCaretPosition();
+		if (control.getSelection() != null) {
+			caretPosition = control.getSelection().x;
+			rangeSelected = true;
+		}
+
+		String textBefore = control.getText();
+
+		// allegmeine Variablen
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(locale);
+		if (insertion.matches("([0-9]*)|([\\" + dfs.getGroupingSeparator() + dfs.getDecimalSeparator() + "]*)")) {
+			Result r = new Result();
+
+			try {
+				r = NumberFormatUtil.processInput(field, insertion, start, end, keyCode, decimals, locale, caretPosition, textBefore, dfs, rangeSelected);
+			} catch (Exception exception) {
+				NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+				r.value = new Value(0.0, field.getDataType());
+				if (field.getDataType().equals(DataType.BIGDECIMAL)) {
+					r.text = numberFormat.format(r.value.getBigDecimalValue());
+				} else {
+					r.text = numberFormat.format(r.value.getDoubleValue());
+				}
+				r.caretPosition = 1;
+			}
+
+			verificationActive = true;
+			field.setValue(r.value, true);
+			control.setText(r.text);
+			control.setSelection(r.caretPosition);
+		}
+		e.doit = false;
+		verificationActive = false;
 	}
 }
