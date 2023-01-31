31a663a3fa952bc67456a9a77d0e6b196edd9dad
bauer <bauer@localhost>
2023-01-31 13:06:14 +0100

    Datensatz mit passendem KeyText über IndexView suchen

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/LoadFromMatchcode.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/LoadFromMatchcode.java
@@ -2,6 +2,8 @@
 package aero.minova.rcp.rcp.handlers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
@@ -13,13 +15,18 @@ import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 import aero.minova.rcp.constants.Constants;
 import aero.minova.rcp.dataservice.IDataFormService;
+import aero.minova.rcp.dataservice.IDataService;
 import aero.minova.rcp.model.Column;
+import aero.minova.rcp.model.DataType;
+import aero.minova.rcp.model.FilterValue;
 import aero.minova.rcp.model.Row;
 import aero.minova.rcp.model.Table;
-import aero.minova.rcp.model.Value;
+import aero.minova.rcp.model.builder.RowBuilder;
+import aero.minova.rcp.model.builder.TableBuilder;
 import aero.minova.rcp.model.form.MField;
 import aero.minova.rcp.rcp.accessor.AbstractValueAccessor;
 import aero.minova.rcp.rcp.parts.WFCDetailPart;
+import aero.minova.rcp.rcp.parts.WFCIndexPart;
 import aero.minova.rcp.rcp.util.WFCDetailCASRequestsUtil;
 
 public class LoadFromMatchcode {
@@ -31,31 +38,69 @@ public class LoadFromMatchcode {
 	private EPartService partService;
 
 	@Inject
+	private IDataService dataService;
+
+	@Inject
 	private IDataFormService dataFormService;
 
+
 	@Execute
-	public void execute(MPerspective mPerspective) {
+	public void execute(MPerspective mPerspective) throws InterruptedException, ExecutionException {
+		MField field = null;
+		String indexViewName;
+
+		// Wir holen uns den DetailPart
 		List<MPart> findElements = model.findElements(mPerspective, Constants.DETAIL_PART, MPart.class);
 		MPart part = findElements.get(0);
 		partService.activate(part);
 		WFCDetailPart detailPart = (WFCDetailPart) part.getObject();
-		MField field = detailPart.getDetail().getField(Constants.TABLE_KEYTEXT);
-		((AbstractValueAccessor) field.getValueAccessor()).getControl().setFocus();
+		indexViewName = detailPart.getForm().getIndexView().getSource();
 
-		Table t = dataFormService.getTableFromFormIndex(detailPart.getForm());
-		Row r = new Row();
-		for (Column c : t.getColumns()) {
-			if(c.getName().equals(Constants.TABLE_KEYTEXT)) {
-				r.addValue(field.getValue());
-			} else {
-				r.addValue(new Value(null, c.getType()));
+		// Wir holen uns den IndexPart
+		findElements = model.findElements(mPerspective, Constants.INDEX_PART, MPart.class);
+		part = findElements.get(0);
+		partService.activate(part);
+		WFCIndexPart indexPart = (WFCIndexPart) part.getObject();
+
+		// Matchcode Feld oder Feld mit dessen Eigenschaften ermitteln (key-type="user")
+		for (MField field2 : detailPart.getDetail().getFields()) {
+			if (field2.isKeyTypeUser() || field2.getName().equals(Constants.TABLE_KEYTEXT)) {
+				field = field2;
 			}
 		}
-		t.addRow(r);
-		
 
-		WFCDetailCASRequestsUtil wDCR = new WFCDetailCASRequestsUtil();
-		wDCR.readData(t);
+		if (field != null) {
+			((AbstractValueAccessor) field.getValueAccessor()).getControl().setFocus();
+
+			// Tabelle für IndexView Aufruf erstellen
+			Table t = TableBuilder.newTable(indexViewName) //
+					.withColumn(Constants.TABLE_KEYLONG, DataType.INTEGER)//
+					.withColumn(Constants.TABLE_KEYTEXT, DataType.STRING)//
+					.create();
+			Row row = RowBuilder.newRow() //
+					.withValue(null) //
+					.withValue(field.getValue().getStringValue()) //
+					.create();
+			t.addRow(row);
+
+			// IndexView aufrufen
+			CompletableFuture<Table> tableFuture = dataService.getTableAsync(t);
+
+			// Tabelle für die Read-Prozedur erstellen
+			Table table = dataFormService.getTableFromFormIndex(indexPart.getForm());
+			Row row2 = RowBuilder.newRow().create();
+			for (Column column : table.getColumns()) {
+				if (column.isKey()) {
+					row2.addValue(tableFuture.get().getValue(column.getName(), 0));
+				} else {
+					row2.addValue(null);
+				}
+			}
+			table.addRow(row2);
+
+			// READ-Prozedur ausführen um Detail zu füllen
+			detailPart.getRequestUtil().readData(table);
+		}
 	}
 
 }
