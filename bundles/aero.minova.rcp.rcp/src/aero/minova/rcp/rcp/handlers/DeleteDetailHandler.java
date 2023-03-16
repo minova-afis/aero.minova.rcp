a96a6d87080a67bd59aa7b8bf50954aed23ef130
Lea Janiak <janiak@minova.aero>
2023-03-16 15:40:17 +0100

    DeleteHanlder passt bei Booking-Typ den Löschen-Knopf an

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/DeleteDetailHandler.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/handlers/DeleteDetailHandler.java
@@ -1,7 +1,6 @@
 package aero.minova.rcp.rcp.handlers;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
@@ -11,12 +10,12 @@ import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.core.services.translation.TranslationService;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
+import org.eclipse.e4.ui.model.application.ui.menu.impl.HandledToolItemImpl;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 
 import aero.minova.rcp.constants.Constants;
-import aero.minova.rcp.model.form.MDetail;
 import aero.minova.rcp.preferences.ApplicationPreferences;
 import aero.minova.rcp.rcp.parts.WFCDetailPart;
 import aero.minova.rcp.rcp.util.DirtyFlagUtil;
@@ -37,23 +36,47 @@ public class DeleteDetailHandler {
 	@Optional
 	DirtyFlagUtil dirtyFlagUtil;
 
+	boolean firstCall = true;
+	MPart mPart;
+	WFCDetailPart detail;
+
 	@CanExecute
-	public boolean canExecute(MPart part, @Named(IServiceConstants.ACTIVE_SELECTION) @Optional Object selection) {
-		if (part.getObject() instanceof WFCDetailPart) {
-			MDetail detail = ((WFCDetailPart) part.getObject()).getDetail();
-			return detail.getPrimaryFields().get(0).getValue() != null;
+	public boolean canExecute(MPart mPart) {
+		if (firstCall) {
+			init(mPart);
 		}
-		return false;
+
+		return detail.getDetail().getPrimaryFields().get(0).getValue() != null;
+	}
+
+	private void init(MPart mPart) {
+		this.mPart = mPart;
+		this.detail = (WFCDetailPart) mPart.getObject();
+
+		// Bei Booking muss das Icon und Label angepasst werden
+		if (detail.getDetail().isBooking()) {
+			for (MToolBarElement item : mPart.getToolbar().getChildren()) {
+				if (Constants.DELETE_DETAIL_BUTTON.equals(item.getElementId())) {
+					HandledToolItemImpl toolItem = (HandledToolItemImpl) item;
+					toolItem.setIconURI(toolItem.getIconURI().replace("DeleteRecord", "CancelBooking"));
+					toolItem.setLabel("@Action.CancleBooking");
+					toolItem.setTooltip("@Action.CancleBooking");
+					break;
+				}
+			}
+		}
+
+		firstCall = false;
 	}
 
 	@Execute
-	public void execute(MPart part, @Optional MPerspective perspective) {
+	public void execute(@Optional MPerspective perspective) {
 		if (perspective == null) {
 			return;
 		}
 
 		// Bei ungespeicherten Änderungen darf nicht gelöscht werden
-		if (part.getObject() instanceof WFCDetailPart && dirtyFlagUtil.isDirty()) {
+		if (mPart.getObject() instanceof WFCDetailPart && dirtyFlagUtil.isDirty()) {
 			MessageDialog dialog = new MessageDialog(Display.getDefault().getActiveShell(),
 					translationService.translate("@msg.DeleteUnsavedChangesTitle", null), null,
 					translationService.translate("@msg.DeleteUnsavedChangesMessage", null), MessageDialog.CONFIRM, new String[] { "OK" }, 0);
