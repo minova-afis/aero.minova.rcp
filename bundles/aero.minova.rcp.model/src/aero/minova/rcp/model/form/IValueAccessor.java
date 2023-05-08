8ef4aa3231ff22b36fa2f0ed431f4838c61651a3
Lea Janiak <janiak@minova.aero>
2023-05-08 16:45:35 +0200

    Methode leichter aufrufbar machen

--- bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/form/IValueAccessor.java
+++ bundles/aero.minova.rcp.model/src/aero/minova/rcp/model/form/IValueAccessor.java
@@ -1,5 +1,6 @@
 package aero.minova.rcp.model.form;
 
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 import aero.minova.rcp.model.LookupValue;
@@ -36,5 +37,15 @@ public interface IValueAccessor {
 	 */
 	void setFilterForLookupContentProvider(Predicate<LookupValue> filter);
 
+	/**
+	 * Setzt einen Comparator, mit dem die Lookup-Values, die angezeigt werden, sortiert werden. Ist kein Comparator gegeben oder setzt man ihn auf null, so
+	 * wird wieder der "Standard" verwendet (alpabethisch ignorierend Groß-/Kleinschreibung, genauer Match auf KeyText als Erstes). Für Nicht-Lookup Felder hat
+	 * die Methode keine Auswirkung
+	 * 
+	 * @param comparator
+	 */
+	void setComparatorForLookupContentProvider(Comparator<LookupValue> comparator);
+
 	void setTooltip(String tooltip);
+
 }
