d0ac467f29a156cccc09821d5748020605c84de5
janiak <janiak@minova.aero>
2024-03-07 10:17:44 +0100

    Klasse in passendes Package verschieben

--- bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/SummaryRowUtil.java
+++ bundles/aero.minova.rcp.rcp/src/aero/minova/rcp/rcp/nattable/SummaryRowUtil.java
@@ -1,4 +1,4 @@
-package aero.minova.rcp.rcp.util;
+package aero.minova.rcp.rcp.nattable;
 
 import java.util.List;
 
@@ -20,9 +20,9 @@ import aero.minova.rcp.model.Row;
 import aero.minova.rcp.nattable.data.MinovaColumnPropertyAccessor;
 import ca.odell.glazedlists.SortedList;
 
-public class NattableSummaryUtil {
+public class SummaryRowUtil {
 
-	private NattableSummaryUtil() {}
+	private SummaryRowUtil() {}
 
 	public static void configureSummary(Form form, NatTable natTable, SortedList<Row> sortedList, MinovaColumnPropertyAccessor columnPropertyAccessor) {
 		List<ColumnWrapper> columns = form.getIndexView().getColumn().stream().map(c -> new ColumnWrapper(c.getAggregate(), c.isTotal())).toList();
